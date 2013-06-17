035593ffa4bf67761b30ca8de46c3f2d12a003d0
Greg Leclercq <greg@sem.io>
2013-06-17 15:22:59 +0200

    Update cdf.streams.utils: add doctests and top-level description

--- cdf/streams/utils.py
+++ cdf/streams/utils.py
@@ -1,12 +1,31 @@
+"""
+A stream is a generator of values. A value may be any object but usually is a
+string or a tuple. The purpose of this module is to provide simple operations
+that are easy to compose.
+
+A stream allow to chain operations on each of its elements and consume the
+values on-demand. Calling a stream operation after another does not iterate two
+times on the values.
+
+""
+from itertools import izip
+
 from cdf.settings import STREAMS_HEADERS
 
 
-__all__ = ['split', 'rstrip', 'split_file']
+__all__ = ['split', 'rstrip', 'split_file', 'idx_from_stream']
 
 
 def split(iterable, char='\t'):
     """
     Split each line with *char*.
+
+    Example:
+
+    >>> split(iter(['a\\tb\\tc'])).next()
+    ['a', 'b', 'c']
+    >>> split(iter(['a,b,c']), ',').next()
+    ['a', 'b', 'c']
 
     """
     return (i.split(char) for i in iterable)
@@ -15,6 +34,12 @@ def split(iterable, char='\t'):
 def rstrip(iterable):
     """
     Strip end-of-line and trailing spaces.
+
+    Example:
+
+    >>> iterable = iter(['foo bar \\n'])
+    >>> rstrip(iterable).next()
+    'foo bar'
 
     """
     for i in iterable:
@@ -26,6 +51,12 @@ def split_file(iterable, char='\t'):
     Strip end-of-line and trailing spaces, then split each line with *char*.
 
     :param iterable: usually a file objects
+
+    Example:
+
+    >>> iterable = iter(['a\\tb\\tc  \\n'])
+    >>> split_file(iterable).next()
+    ['a', 'b', 'c']
 
     """
     return split(rstrip(iterable))
