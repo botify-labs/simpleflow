95d9af22773eb73746796e4717853789ad834881
ampelmann <thomas@sem.io>
2013-06-18 17:49:11 +0200

    add validator for resource_type settings (in  collection)

---
+++ cdf/collections/url_properties/generator.py
@@ -0,0 +1,5 @@
+class UrlPropertiesGenerator(object):
+
+    def __init__(self, stream_patterns, resource_type_settings):
+        self.stream_patterns = stream_patterns
+        self.resource_type_settings = resource_type_settings
