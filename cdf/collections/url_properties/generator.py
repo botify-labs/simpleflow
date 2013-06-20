8b9baec9d293b85c2c43708c09d4fbb0b80b4a60
ampelmann <thomas@sem.io>
2013-06-20 19:55:26 +0200

    implements UrlPropertiesGenerator

--- cdf/collections/url_properties/generator.py
+++ cdf/collections/url_properties/generator.py
@@ -1,4 +1,4 @@
-from cdf.collection.url_properties.validator import ResourceTypeValidator
+from cdf.collections.url_properties.resource_type import compile_resource_type_settings
 
 
 class UrlPropertiesGenerator(object):
@@ -6,18 +6,34 @@ class UrlPropertiesGenerator(object):
     def __init__(self, stream_patterns, resource_type_settings):
         self.stream_patterns = stream_patterns
         self.resource_type_settings = resource_type_settings
-        self.resource_type_validator = ResourceTypeValidator(resource_type_settings)
+        self.resource_type_compiled = compile_resource_type_settings(self.resource_type_settings)
 
     def is_valid(self):
         return self.resource_type_validator.is_valid()
 
-    """
-    Return a dict with the following format : 
-    {
-        "resource_type": "value"
-    }
-    """
     def __iter__(self):
-        for i in self.streams_patterns:
+        """
+        Return a tuple where first value is the url_id and second value is a dict
+        Ex :
+        (1, {
+            "resource_type": "value"
+        }
+        """
+        for i in self.stream_patterns:
             url_id, protocol, host, path, query_string = i
-            url = ''.join((protocol, host, path, query_string))
+            locator = '' # not yet in urlids.txt
+            parents_matches = set()
+            for rule in self.resource_type_compiled:
+                """
+                If the rule inherits from another one but the url did not match
+                with the parent, we pass
+                """
+                if 'inherits_from' in rule and rule['inherits_from'] not in parents_matches:
+                    continue
+
+                if rule['query'](protocol, host, path, query_string, locator):
+                    if not rule.get('abstract', False):
+                        yield (url_id, {"resource_type": rule['value']})
+                    else:
+                        parents_matches.add(rule['rule_id'])
+            yield (url_id, {"resource_type": "unkown"})
---
+++ tests/collections/url_properties/test_generator.py
@@ -0,0 +1,114 @@
+# -*- coding:utf-8 -*-
+import unittest
+import logging
+from datetime import datetime
+
+
+from cdf.log import logger
+from cdf.collections.url_properties.generator import UrlPropertiesGenerator
+
+logger.setLevel(logging.DEBUG)
+
+
+class TestUrlDataGenerator(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_simple(self):
+        patterns = (
+            [1, 'http', 'www.site.com', '/path/name.html', ''],
+        )
+
+        resource_type_settings = {
+            '*.site.com': [
+                {'query': "STARTS(path, '/path')",
+                 'value': "mypath"
+                 }
+            ]
+        }
+
+        u = UrlPropertiesGenerator(iter(patterns), resource_type_settings)
+        results = list(u)
+        self.assertEquals(results[0], (1, {"resource_type": "mypath"}))
+
+    def test_unkown(self):
+        patterns = (
+            [1, 'http', 'www.site.com', '/path/name.html', ''],
+        )
+
+        resource_type_settings = {
+            '*.site.com': [
+                {'query': "STARTS(path, '/another_path')",
+                 'value': "mypath"
+                 }
+            ]
+        }
+
+        u = UrlPropertiesGenerator(iter(patterns), resource_type_settings)
+        results = list(u)
+        self.assertEquals(results[0], (1, {"resource_type": "unkown"}))
+
+    def test_abstract(self):
+        """
+        The url matches with the first rule but as it has been abstracted, it should be unkown
+        """
+        patterns = (
+            [1, 'http', 'www.site.com', '/path/name.html', ''],
+        )
+
+        resource_type_settings = {
+            '*.site.com': [
+                {'query': "STARTS(path, '/path')",
+                 'abstract': True,
+                 'rule_id': 'path_rule'
+                 },
+                {'query': "ENDS(path, '.json')",
+                 'value': 'json',
+                 'inherits_from': 'path_rule'
+                 }
+            ]
+        }
+
+        u = UrlPropertiesGenerator(iter(patterns), resource_type_settings)
+        results = list(u)
+        self.assertEquals(results[0], (1, {"resource_type": "unkown"}))
+
+        # Now we had another rule which should match
+        resource_type_settings['*.site.com'].append({
+            'query': "ENDS(path, '.html')",
+            'value': 'html',
+            'inherits_from': 'path_rule'
+        })
+
+        u = UrlPropertiesGenerator(iter(patterns), resource_type_settings)
+        results = list(u)
+        self.assertEquals(results[0], (1, {"resource_type": "html"}))
+
+    def test_inherits(self):
+        """
+        The url matches with the second rule but as it inherits from the first one, it should not match.
+        """
+        patterns = (
+            [1, 'http', 'www.site.com', '/music/name.html', ''],
+        )
+
+        resource_type_settings = {
+            '*.site.com': [
+                {'query': "STARTS(path, '/movie')",
+                 'abstract': True,
+                 'rule_id': 'movie_rule'
+                 },
+                {'query': "ENDS(path, '.json')",
+                 'value': 'movie/json',
+                 'inherits_from': 'movie_rule'
+                 }
+            ]
+        }
+
+        u = UrlPropertiesGenerator(iter(patterns), resource_type_settings)
+        results = list(u)
+        self.assertEquals(results[0], (1, {"resource_type": "unkown"}))
