160be0394e9fe3a5b6ee877db0ff46504bb8ffb0
ampelmann <thomas@sem.io>
2013-06-21 13:07:34 +0200

    fix UrlPropertiesGenerator

--- cdf/collections/url_properties/generator.py
+++ cdf/collections/url_properties/generator.py
@@ -8,9 +8,6 @@ class UrlPropertiesGenerator(object):
         self.resource_type_settings = resource_type_settings
         self.resource_type_compiled = compile_resource_type_settings(self.resource_type_settings)
 
-    def is_valid(self):
-        return self.resource_type_validator.is_valid()
-
     def __iter__(self):
         """
         Return a tuple where first value is the url_id and second value is a dict
@@ -21,8 +18,12 @@ class UrlPropertiesGenerator(object):
         """
         for i in self.stream_patterns:
             url_id, protocol, host, path, query_string = i
-            locator = '' # not yet in urlids.txt
+            # localtor not yet in urlids.txt
+            locator = ''
+
             parents_matches = set()
+            found = False
+
             for rule in self.resource_type_compiled:
                 """
                 If the rule inherits from another one but the url did not match
@@ -33,7 +34,10 @@ class UrlPropertiesGenerator(object):
 
                 if rule['query'](protocol, host, path, query_string, locator):
                     if not rule.get('abstract', False):
+                        found = True
                         yield (url_id, {"resource_type": rule['value']})
+                        break
                     else:
                         parents_matches.add(rule['rule_id'])
-            yield (url_id, {"resource_type": "unkown"})
+            if not found:
+                yield (url_id, {"resource_type": "unkown"})
