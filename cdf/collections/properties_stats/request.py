6ae4c3d35ebbafc5a67c5b53b97c2afd347b507f
ampelmann <thomas@sem.io>
2013-07-03 10:28:38 +0200

    change request filters to map with an json request

--- cdf/collections/properties_stats/request.py
+++ cdf/collections/properties_stats/request.py
@@ -1,9 +1,22 @@
 import os
+import re
+import operator
 from pandas import HDFStore
 
 from cdf.collections.properties_stats.constants import CROSS_PROPERTIES_COLUMNS, CROSS_PROPERTIES_META_COLUMNS
 
 from cdf.utils.s3 import fetch_files
+
+
+def is_dict_filter(filter_dict):
+    """
+    Check if the incoming dict is a filter (means "field" et "value" keys are set
+    """
+    return 'field' in filter_dict and 'value' in filter_dict
+
+
+def is_boolean_operation_filter(filter_dict):
+    return len(filter_dict) == 1 and filter_dict.keys()[0].lower() in ('and', 'or')
 
 
 class CounterRequest(object):
@@ -19,80 +32,208 @@ class CounterRequest(object):
         store = HDFStore(files_fetched[0][0])
         return cls(store[cls.STORE_KEY])
 
-    def df_from_filters(self, filters):
-        df = self.df.copy()
-        for key, value in filters.iteritems():
-            if callable(value):
-                df = df[value(df[key])]
+    def _transform_filter_value(self, value):
+        """
+        This function is called to create filters from `resource_type` and `host`
+        Wildcards (*) are replaced by a regular expression to make the check
+        """
+        pattern = "^%s$" % value.replace('*', '([\w-]+)')
+        return pattern
+
+    def get_func_from_filter_dict(self, df, _filter):
+        if isinstance(_filter['value'], list):
+            _clean_filters = map(lambda i: self._transform_filter_value(i), _filter['value'])
+            return df[_filter['field']].map(lambda i: any(bool(re.search(_v, i)) for _v in _clean_filters))
+        else:
+            return df[_filter['field']].map(lambda i: bool(re.search(self._transform_filter_value(_filter['value']), i)))
+
+    def _apply_filters_list(self, df, lst, _operator='or'):
+        filters_func = None
+        for _filter in lst:
+            if filters_func is None:
+                filters_func = self.get_func_from_filter_dict(df, _filter)
             else:
-                df = df[df[key] == value]
-        return df
+                getattr(operator, '__i%s__' % _operator)(filters_func, self.get_func_from_filter_dict(df, _filter))
+        return filters_func
 
-    """
-    Return the total sum from a list of `fields`
+    def _apply_filters(self, df, filters, parent_operator="or"):
+        filters_func = None
+        if isinstance(filters, list):
+            if filters[0].keys()[0].lower() in ('and', 'or'):
+                _sub_operator_key = filters[0].keys()[0]
+                filters_func = self._apply_filters_list(df, filters[0][_sub_operator_key], _operator=_sub_operator_key.lower())
+            else:
+                filters_func = self._apply_filters_list(df, filters, _operator=parent_operator)
+        elif is_dict_filter(filters):
+            return self.get_func_from_filter_dict(df, filters)
+        elif is_boolean_operation_filter(filters):
+            key = filters.keys()[0]
+            _operator = key.lower()
 
-    :param fields : a list of fields
-    :param filters : a dictionnary where key is in DISTRIBUTION_COLUMNS and value is a string or a function to apply on this dataframe's field
+            for _filter in filters[key]:
+                if filters_func is None:
+                    filters_func = self._apply_filters(df, _filter, parent_operator=_operator)
+                else:
+                    getattr(operator, "__i%s__" % _operator)(filters_func, self._apply_filters(df, _filter, parent_operator=_operator))
+        else:
+            raise Exception('Filter not well formated : %s' % filters)
+        return filters_func
 
-    Return a dict with the `field` as key and sum as value
-    """
-    def fields_sum(self, fields, filters=None):
-        if filters and any(f not in self.DISTRIBUTION_COLUMNS for f in filters.keys()):
-            raise Exception('One of the submitted filters is not allowed')
-        df = self.df_from_filters(filters) if filters else self.df
-        return {f: int(df[f].sum()) for f in fields}
+    def fields_sum(self, settings):
+        """
+        Return the total sum from a list of `fields`
 
-    """
-    Return the total sum from a list of `fields` aggregated by cross-property
 
-    :param fields : a list of files
-    :param filters : a dictionnary where key in DISTRIBUTION_COLUMNS and value is a string or a function to apply on this dataframe's field
-    :params merge : if None, return all properties combinations possible. If a tuple of properties, return all combination matching those fields
+        :param settings
+        :param resource_type : a list of resource types. wildcards (*) are allowed at the start or at the end of each resource_type
+        :param _filters : a list of defined filters (coming from a parent class)
 
-    Return a list of dictionaries with two keys "properties" and "counters".
+        Return a dict with the `field` as key and sum as value
+        """
+        if 'fields' in settings:
+            fields = settings['fields']
+        else:
+            fields = ['pages_nb']
 
-    Ex :
+        if 'filters' in settings:
+            df = self.df.copy()
+            df = df[self._apply_filters(df, settings['filters'])]
+        else:
+            df = self.df
+        return {f: int(df[f].sum()) for f in settings['fields']}
 
-    [
-        {
-            "properties": {
-                "host": "www.site.com",
-                "content_type": "text/html"
+    def fields_sum_by_property(self, settings):
+        """
+        Return the total sum from a list of `fields` aggregated by cross-property
+
+        :param fields : a list of files
+        :param resource_type : a list of resource types. wildcards (*) are allowed at the start or at the end of each resource_type
+        :params group_by : if None, return all properties combinations possible. If a tuple of properties, return all combination matching those fields
+        :param _filters : a list of defined filters (coming from a parent class)
+
+        Return a list of dictionaries with two keys "properties" and "counters".
+
+        Ex :
+
+        [
+            {
+                "properties": {
+                    "host": "www.site.com",
+                    "content_type": "text/html"
+                },
+                "counters": {
+                    "pages_nb": 10,
+                }
             },
-            "counters": {
-                "pages_nb": 10,
+            {
+                "properties": {
+                    "host": "subdomain.site.com",
+                    "content_type": "text/html"
+                },
+                "counters": {
+                    "pages_nb": 20
+                }
             }
-        },
-        {
-            "properties": {
-                "host": "subdomain.site.com",
-                "content_type": "text/html"
-            },
-            "counters": {
-                "pages_nb": 20
-            }
-        }
-    ]
-    """
-    def fields_sum_by_property(self, fields, filters=None, merge=False):
+        ]
+        """
         results = {}
-        if not merge:
-            merge = self.DISTRIBUTION_COLUMNS
-        df = self.df_from_filters(filters) if filters else self.df
-        df = df.groupby(merge).reset_index()
+        if 'group_by' in settings:
+            group_by = settings['group_by']
+        else:
+            group_by = self.DISTRIBUTION_COLUMNS
+
+        if 'fields' in settings:
+            fields = settings['fields']
+        else:
+            fields = ['pages_nb']
+
+        df = self.df.copy()
+        if 'filters' in settings:
+            df = df[self._apply_filters(df, settings['filters'])]
+        df = self._map_host_group_by(df, group_by)
+        df = self._map_resource_type_group_by(df, group_by)
+        df = df.groupby(group_by).agg('sum').reset_index()
         results = []
         for i, n in enumerate(df.values):
             result = {
-                'properties': {field_: df[field_][i] for field_ in merge},
-                'counters': {field_: df[field_][i] for field_ in fields}
+                'properties': {field_: df[field_][i] for field_ in group_by},
+                'counters': {field_: int(df[field_][i]) if df[field_][i] > 0 else 0 for field_ in fields}
             }
             results.append(result)
         return results
+
+    def _map_host_group_by(self, df, group_by):
+        hosts = filter(lambda i: i.startswith('host__level'), group_by)
+        if len(hosts) == 1:
+            try:
+                hostname = hosts[0]
+                level = int(hostname[len('host__level')])
+            except:
+                raise Exception("Level has to be an integer (ex host__level1)")
+            group_by.remove(hostname)
+            group_by.append('host')
+
+            def host_to_level(host, level):
+                splited = host.rsplit('.', level)
+                if len(splited) > level:
+                    return '*.' + '.'.join(splited[-level:])
+                return '.'.join(splited[-level:])
+
+            df['host'] = df['host'].map(lambda name: host_to_level(name, level))
+        elif len(hosts) > 1:
+            raise Exception("It is not allowed to make a group_by with differents host's levels")
+        return df
+
+    def _map_resource_type_group_by(self, df, group_by):
+        rt_ = filter(lambda i: i.startswith('resource_type__level'), group_by)
+        if len(rt_) == 1:
+            try:
+                resource_type = rt_[0]
+                level = int(resource_type[len('resource_type__level')])
+            except:
+                raise Exception("Level has to be an integer (ex resource_type__level1)")
+            group_by.remove(resource_type)
+            group_by.append('resource_type')
+
+            def rename_resource_type(name, level):
+                splited = name.split('/', level)
+                if len(splited) > level:
+                    return '/'.join(splited[:level]) + '/*'
+                return name
+
+            df['resource_type'] = df['resource_type'].map(lambda name: rename_resource_type(name, level))
+        elif len(rt_) > 1:
+            raise Exception("It is not allowed to make a group_by with differents resourc_type's levels")
+        return df
 
 
 class PropertiesStatsRequest(CounterRequest):
     DISTRIBUTION_COLUMNS = CROSS_PROPERTIES_COLUMNS
     STORE_KEY = 'counter'
+
+    """
+    def fields_sum(self, fields, resource_type=None, host=None, min_depth=None, max_depth=None):
+        if min_depth or max_depth:
+            _filters = [('depth', self._get_depth_filter(min_depth, max_depth))]
+        else:
+            _filters = None
+        return super(PropertiesStatsRequest, self).fields_sum(fields, resource_type, host, _filters=_filters)
+
+    def fields_sum_by_property(self, fields, resource_type=None, host=None, group_by=False, min_depth=None, max_depth=None):
+        if min_depth or max_depth:
+            _filters = [('depth', self._get_depth_filter(min_depth, max_depth))]
+        else:
+            _filters = None
+        return super(PropertiesStatsRequest, self).fields_sum_by_property(fields, resource_type, host, group_by, _filters=_filters)
+
+    def _get_depth_filter(self, min_depth, max_depth):
+        if min_depth and max_depth:
+            return lambda i: i in range(min_depth, max_depth + 1)
+        elif min_depth:
+            return lambda i: i >= min_depth
+        elif max_depth:
+            return lambda i: i <= max_depth
+    """
 
 
 class PropertiesStatsMetaRequest(CounterRequest):
--- tests/collections/properties_stats/test_request.py
+++ tests/collections/properties_stats/test_request.py
@@ -40,22 +40,96 @@ class TestPropertiesStats(unittest.TestCase):
     def test_simple(self):
         df = DataFrame(self.data)
         request = PropertiesStatsRequest(df)
-        self.assertEquals(request.fields_sum(['pages_nb', 'pages_code_200', 'pages_code_500']), {'pages_code_500': 20, 'pages_code_200': 4, 'pages_nb': 30})
 
-        filter_1 = {
-            'host': 'www.site.com'
+        settings = {
+            'fields': ['pages_nb', 'pages_code_200', 'pages_code_500']
         }
-        self.assertEquals(request.fields_sum(['pages_nb'], filter_1), {'pages_nb': 10})
 
-        filter_2 = {
-            'host': lambda i: i.str.endswith('.site.com')
+        self.assertEquals(request.fields_sum(settings), {'pages_code_500': 20, 'pages_code_200': 4, 'pages_nb': 30})
+        settings = {
+            'fields': ['pages_nb'],
+            'filters': [
+                {'field': 'host', 'value': 'www.site.com'}
+            ]
         }
-        self.assertEquals(request.fields_sum(['pages_nb'], filter_2), {'pages_nb': 30})
+        self.assertEquals(request.fields_sum(settings), {'pages_nb': 10})
 
-        filter_3 = {
-            'depth': lambda i: i.isin([1, 4])
+        # Implicit OR
+        settings = {
+            'fields': ['pages_nb'],
+            'filters': [
+                {'field': 'host', 'value': 'www.site.com'},
+                {'field': 'host', 'value': 'subdomain.site.com'}
+            ]
         }
-        self.assertEquals(request.fields_sum(['pages_nb'], filter_3), {'pages_nb': 10})
+        self.assertEquals(request.fields_sum(settings), {'pages_nb': 30})
+
+        # Wildcart at start
+        settings['filters'] = [
+            {'field': 'host', 'value': '*.site.com'}
+        ]
+        self.assertEquals(request.fields_sum(settings), {'pages_nb': 30})
+
+        # Wildcard at start and at end
+        settings['filters'] = [
+            {'field': 'host', 'value': '*.site.*'}
+        ]
+        self.assertEquals(request.fields_sum(settings), {'pages_nb': 30})
+
+        # explicit OR condition
+        settings['filters'] = {
+            'or': [
+                {'field': 'host', 'value': 'www.site.com'},
+                {'field': 'host', 'value': 'subdomain.site.com'}
+            ]
+        }
+        self.assertEquals(request.fields_sum(settings), {'pages_nb': 30})
+
+        # AND condition
+        settings['filters'] = {
+            'and': [
+                {'field': 'host', 'value': '*.site.com'},
+                {'field': 'resource_type', 'value': 'article'}
+            ]
+        }
+        self.assertEquals(request.fields_sum(settings), {'pages_nb': 10})
+
+    def test_nested_filter(self):
+        df = DataFrame(self.data)
+        request = PropertiesStatsRequest(df)
+
+        settings = {
+            'fields': ['pages_nb']
+        }
+
+        # nested OR + AND conditions
+        settings['filters'] = {
+            'or': [
+                {'and': [
+                    {'field': 'host', 'value': 'www.site.com'},
+                    {'field': 'resource_type', 'value': 'article'}
+                ]},
+                {'and': [
+                    {'field': 'host', 'value': 'subdomain.site.com'},
+                    {'field': 'resource_type', 'value': 'photo'}
+                ]}
+            ]
+        }
+        self.assertEquals(request.fields_sum(settings), {'pages_nb': 30})
+
+    def test_filter_in(self):
+        df = DataFrame(self.data)
+        request = PropertiesStatsRequest(df)
+
+        settings = {
+            'fields': ['pages_nb']
+        }
+
+        # nested OR + AND conditions
+        settings['filters'] = [
+            {'field': 'host', 'value': ['www.site.com', 'subdomain.site.com']}
+        ]
+        self.assertEquals(request.fields_sum(settings), {'pages_nb': 30})
 
     def test_sum_by_property_1dim(self):
         df = DataFrame(self.data)
@@ -79,7 +153,11 @@ class TestPropertiesStats(unittest.TestCase):
                 }
             }
         ]
-        self.assertItemsEqual(request.fields_sum_by_property(['pages_nb'], merge=['host']), expected_results)
+        settings = {
+            'fields': ['pages_nb'],
+            'group_by': ['host']
+        }
+        self.assertItemsEqual(request.fields_sum_by_property(settings), expected_results)
 
     def test_sum_by_property_2dim(self):
         df = DataFrame(self.data)
@@ -105,4 +183,268 @@ class TestPropertiesStats(unittest.TestCase):
                 }
             }
         ]
-        self.assertItemsEqual(request.fields_sum_by_property(['pages_nb'], merge=['host', 'content_type']), expected_results)
+        settings = {
+            'fields': ['pages_nb'],
+            'group_by': ['host', 'content_type']
+        }
+        self.assertItemsEqual(request.fields_sum_by_property(settings), expected_results)
+
+    def test_sum_domains(self):
+        self.data = [
+            {'host': 'www.site.com',
+             'content_type': 'text/html',
+             'resource_type': 'article',
+             'depth': 1,
+             'follow': True,
+             'index': True,
+             'pages_nb': 10,
+             },
+            {'host': 'my.subdomain.site.com',
+             'content_type': 'text/html',
+             'resource_type': 'photo',
+             'depth': 5,
+             'follow': True,
+             'index': True,
+             'pages_nb': 20,
+             },
+            {'host': 'www.site.fr',
+             'content_type': 'text/html',
+             'resource_type': 'product',
+             'depth': 1,
+             'follow': True,
+             'index': True,
+             'pages_nb': 40,
+             }
+        ]
+
+        df = DataFrame(self.data)
+        request = PropertiesStatsRequest(df)
+
+        # Level 1, TLDs
+        expected_results = [
+            {
+                "properties": {
+                    "host": "*.com",
+                },
+                "counters": {
+                    "pages_nb": 30,
+                }
+            },
+            {
+                "properties": {
+                    "host": "*.fr",
+                },
+                "counters": {
+                    "pages_nb": 40
+                }
+            }
+        ]
+        settings = {
+            'fields': ['pages_nb'],
+            'group_by': ['host__level1']
+        }
+        self.assertItemsEqual(request.fields_sum_by_property(settings), expected_results)
+
+        # Level 2, main domains
+        expected_results = [
+            {
+                "properties": {
+                    "host": "*.site.com",
+                },
+                "counters": {
+                    "pages_nb": 30,
+                }
+            },
+            {
+                "properties": {
+                    "host": "*.site.fr",
+                },
+                "counters": {
+                    "pages_nb": 40
+                }
+            }
+        ]
+        settings = {
+            'fields': ['pages_nb'],
+            'group_by': ['host__level2']
+        }
+        self.assertItemsEqual(request.fields_sum_by_property(settings), expected_results)
+
+        # Level 3, subdomains
+        expected_results = [
+            {
+                "properties": {
+                    "host": "www.site.com",
+                },
+                "counters": {
+                    "pages_nb": 10,
+                }
+            },
+            {
+                "properties": {
+                    "host": "*.subdomain.site.com",
+                },
+                "counters": {
+                    "pages_nb": 20,
+                }
+            },
+            {
+                "properties": {
+                    "host": "www.site.fr",
+                },
+                "counters": {
+                    "pages_nb": 40
+                }
+            }
+        ]
+
+        settings = {
+            'fields': ['pages_nb'],
+            'group_by': ['host__level3']
+        }
+        self.assertItemsEqual(request.fields_sum_by_property(settings), expected_results)
+
+        # Level 4, my.subdomain.site.com
+        expected_results = [
+            {
+                "properties": {
+                    "host": "www.site.com",
+                },
+                "counters": {
+                    "pages_nb": 10,
+                }
+            },
+            {
+                "properties": {
+                    "host": "my.subdomain.site.com",
+                },
+                "counters": {
+                    "pages_nb": 20,
+                }
+            },
+            {
+                "properties": {
+                    "host": "www.site.fr",
+                },
+                "counters": {
+                    "pages_nb": 40
+                }
+            }
+        ]
+        settings = {
+            'fields': ['pages_nb'],
+            'group_by': ['host__level4']
+        }
+        self.assertItemsEqual(request.fields_sum_by_property(settings), expected_results)
+
+    def test_resource_type_levels(self):
+        self.data = [
+            {'host': 'www.site.com',
+             'content_type': 'text/html',
+             'resource_type': 'article/comments/p1',
+             'depth': 1,
+             'follow': True,
+             'index': True,
+             'pages_nb': 10,
+             },
+            {'host': 'www.site.com',
+             'content_type': 'text/html',
+             'resource_type': 'article/comments/px',
+             'depth': 5,
+             'follow': True,
+             'index': True,
+             'pages_nb': 20,
+             },
+            {'host': 'www.site.fr',
+             'content_type': 'text/html',
+             'resource_type': 'article/permalink',
+             'depth': 1,
+             'follow': True,
+             'index': True,
+             'pages_nb': 40,
+             }
+        ]
+
+        df = DataFrame(self.data)
+        request = PropertiesStatsRequest(df)
+
+        # Level 1
+        expected_results = [
+            {
+                "properties": {
+                    "resource_type": "article/*",
+                },
+                "counters": {
+                    "pages_nb": 70,
+                }
+            }
+        ]
+        settings = {
+            'fields': ['pages_nb'],
+            'group_by': ['resource_type__level1']
+        }
+        self.assertItemsEqual(request.fields_sum_by_property(settings), expected_results)
+
+        # Level 2
+        expected_results = [
+            {
+                "properties": {
+                    "resource_type": "article/comments/*",
+                },
+                "counters": {
+                    "pages_nb": 30,
+                }
+            },
+            {
+                "properties": {
+                    "resource_type": "article/permalink",
+                },
+                "counters": {
+                    "pages_nb": 40,
+                }
+            }
+        ]
+        settings = {
+            'fields': ['pages_nb'],
+            'group_by': ['resource_type__level2']
+        }
+        self.assertItemsEqual(request.fields_sum_by_property(settings), expected_results)
+
+        # Level 3
+        expected_results = [
+            {
+                "properties": {
+                    "resource_type": "article/comments/p1",
+                },
+                "counters": {
+                    "pages_nb": 10,
+                }
+            },
+            {
+                "properties": {
+                    "resource_type": "article/comments/px",
+                },
+                "counters": {
+                    "pages_nb": 20,
+                }
+            },
+            {
+                "properties": {
+                    "resource_type": "article/permalink",
+                },
+                "counters": {
+                    "pages_nb": 40,
+                }
+            }
+        ]
+        settings = {
+            'fields': ['pages_nb'],
+            'group_by': ['resource_type__level3']
+        }
+        self.assertItemsEqual(request.fields_sum_by_property(settings), expected_results)
+
+        settings = {
+            'fields': ['pages_nb'],
+            'group_by': ['resource_type__level4']
+        }
+        self.assertItemsEqual(request.fields_sum_by_property(settings), expected_results)
