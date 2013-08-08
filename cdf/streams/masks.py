23789062600d64e1e1ef76d50db524ea3cde65a9
ampelmann <thomas@sem.io>
2013-08-08 11:57:58 +0200

    fix follow links mask

--- cdf/streams/masks.py
+++ cdf/streams/masks.py
@@ -1,6 +1,5 @@
 # Order of masks is important !
 FOLLOW_MASKS = [
-    (0, "follow"),
     (8, "config_nofollow"),
     (4, "robots_nofollow"),
     (2, "meta_nofollow"),
@@ -9,9 +8,13 @@ FOLLOW_MASKS = [
 
 
 def follow_mask(val):
-    mask = int(val)
+    """
+    In raw files, an url can be at the same time as "robots_no_follow" and "meta_no_follow"
+    For the front report, we consider that the url has only 1 status. FOLLOW_MASKS constant follow the most to the less important
+    """
+    if val == '0':
+        return "follow"
+    _mask = int(val)
     for bitmask, term in FOLLOW_MASKS:
-        if bitmask & mask == bitmask:
+        if bitmask & _mask == bitmask:
             return term
-
-
