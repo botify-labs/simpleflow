b4203124c7d8e5426d1f71a07e1ec65c5a92e39d
ampelmann <thomas@sem.io>
2013-09-19 11:16:15 +0200

    do no parse part_ids when urls are not crawled

--- cdf/tasks/url_properties.py
+++ cdf/tasks/url_properties.py
@@ -88,20 +88,31 @@ def compute_properties_from_s3(crawl_id, part_id, rev_num, s3_uri, settings, es_
 def compute_properties_stats_counter_from_s3(crawl_id, part_id, rev_num, s3_uri, tmp_dir_prefix='/tmp', force_fetch=False):
     # Fetch locally the files from S3
     tmp_dir = os.path.join(tmp_dir_prefix, 'crawl_%d' % crawl_id)
+    streams = {}
+
+    properties_file = fetch_files(
+        s3_uri,
+        tmp_dir,
+        regexp=['url_properties.rev%d.txt.%d.lz4' % (rev_num, part_id)],
+        force_fetch=force_fetch)
+
+    path_local, fetch = properties_file[0]
+    file_content = lz4.loads(open(path_local).read())
+    if not file_content:
+        return
+    else:
+        cast = Caster(STREAMS_HEADERS["PROPERTIES"]).cast
+        streams["stream_properties"] = cast(split(file_content.split('\n')))
 
     files_fetched = fetch_files(s3_uri,
                                 tmp_dir,
-                                regexp=['url(ids|infos|links|inlinks).txt.%d.gz' % part_id, 'url_properties.rev%d.txt.%d.lz4' % (rev_num, part_id)],
+                                regexp=['url(ids|infos|links|inlinks).txt.%d.gz' % part_id],
                                 force_fetch=force_fetch)
-    streams = {}
 
     for path_local, fetched in files_fetched:
         stream_identifier = STREAMS_FILES[os.path.basename(path_local).split('.')[0]]
         cast = Caster(STREAMS_HEADERS[stream_identifier.upper()]).cast
-        if stream_identifier == "properties":
-            streams["stream_properties"] = cast(split(lz4.loads(open(path_local).read()).split('\n')))
-        else:
-            streams["stream_%s" % stream_identifier] = cast(split_file(gzip.open(path_local)))
+        streams["stream_%s" % stream_identifier] = cast(split_file(gzip.open(path_local)))
 
     # Not crawled urls may be referenced in urlids and urlinfos files, but some files may be missing like inlinks...
     for optional_stream in ('outlinks', 'inlinks'):
@@ -150,19 +161,30 @@ def _get_df_properties_stats_meta_from_s3(crawl_id, rev_num, s3_uri, tmp_dir_pre
                      }
 
     for part_id in xrange(0, nb_parts_from_crawl_location(s3_uri)):
+        properties_file = fetch_files(
+            s3_uri,
+            tmp_dir,
+            regexp=['url_properties.rev%d.txt.%d.lz4' % (rev_num, part_id)],
+            force_fetch=force_fetch)
+
+        path_local, fetch = properties_file[0]
+        file_content = lz4.loads(open(path_local).read())
+        if not file_content:
+            # If no content for properties file, that means that no pages were crawled for this part_id, we can skip
+            continue
+        else:
+            cast = Caster(STREAMS_HEADERS["PROPERTIES"]).cast
+            streams_types["properties"].append(cast(split(file_content.split('\n'))))
+
         files_fetched = fetch_files(s3_uri,
                                     tmp_dir,
-                                    regexp=['url(ids|contents|infos).txt.%d.gz' % part_id, 'url_properties.rev%d.txt.%d.lz4' % (rev_num, part_id)],
+                                    regexp=['url(ids|contents|infos).txt.%d.gz' % part_id],
                                     force_fetch=force_fetch)
 
         for path_local, fetched in files_fetched:
             stream_identifier = STREAMS_FILES[os.path.basename(path_local).split('.')[0]]
             cast = Caster(STREAMS_HEADERS[stream_identifier.upper()]).cast
-
-            if stream_identifier == "properties":
-                streams_types[stream_identifier].append(cast(split(lz4.loads(open(path_local).read()).split('\n'))))
-            else:
-                streams_types[stream_identifier].append(cast(split_file(gzip.open(path_local))))
+            streams_types[stream_identifier].append(cast(split_file(gzip.open(path_local))))
 
     a = MetadataAggregator(itertools.chain(*streams_types['patterns']),
                            itertools.chain(*streams_types['properties']),
