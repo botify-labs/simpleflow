181019b7232569250b3694cd7b5eae535fdd28ba
ampelmann <thomas@sem.io>
2013-10-15 10:53:29 +0200

    merge patterns and metadata suggestions mapping into a single class

--- cdf/collections/urls/generators/suggestions.py
+++ cdf/collections/urls/generators/suggestions.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from cdf.streams.mapping import CONTENT_TYPE_INDEX, CONTENT_TYPE_NAME_TO_ID
+from cdf.streams.utils import idx_from_stream, group_left
 from BQL.parser.tagging import query_to_python
 from BQL.parser.metadata import query_to_python as metadata_query_to_python
 
@@ -14,46 +15,49 @@ def transform_queries(queries_lst, func=query_to_python):
 
 class UrlSuggestionsGenerator(object):
 
-    def __init__(self, stream_patterns, clusters_dict):
+    def __init__(self, stream_patterns, stream_infos, stream_contents):
         self.stream_patterns = stream_patterns
-        self.clusters_dict = {name: transform_queries(queries) for name, queries in clusters_dict.iteritems()}
+        self.stream_infos = stream_infos
+        self.stream_contents = stream_contents
+        self.patterns_clusters = dict()
+        self.metadata_clusters = dict()
+
+    def add_pattern_cluster(self, pattern_name, cluster_list):
+        self.patterns_clusters[pattern_name] = transform_queries(cluster_list, query_to_python)
+
+    def add_metadata_cluster(self, metadata_type, cluster_list):
+        if metadata_type not in CONTENT_TYPE_INDEX.values():
+            raise Exception('{} is not a valid metadata type'.format(metadata_type))
+        self.metadata_clusters[CONTENT_TYPE_NAME_TO_ID[metadata_type]] = transform_queries(cluster_list, metadata_query_to_python)
 
     def __iter__(self):
-        while True:
-            try:
-                entry = self.stream_patterns.next()
-            except StopIteration:
-                break
-            url_id, protocol, host, path, query_string = entry
+        http_code_idx = idx_from_stream('infos', 'http_code')
+        for i in group_left((self.stream_patterns, 0), infos=(self.stream_infos, 0), contents=(self.stream_contents, 0)):
+
+            url_id, left_line, streams = i
+            # If http_code in 0, 1, 2 > means than not crawled
+            # If http_code < 0, it means that there were a fetcher error but the url was correctly crawled
+            if streams['infos'][0][http_code_idx] in (0, 1, 2):
+                continue
+
+            url_id, protocol, host, path, query_string = left_line
             # locator not yet in urlids.txt
             locator = ''
             url = "{}://{}{}{}".format(protocol, host, path, query_string)
 
-            for cluster, queries in self.clusters_dict.iteritems():
+            for cluster, queries in self.patterns_clusters.iteritems():
                 for query in queries:
                     if query['func'](url, protocol, host, path, query_string, locator):
                         yield (url_id, cluster, query['string'])
+
+            for entry in streams['contents']:
+                url_id, metadata_type, hash_id, value = entry
+                for query in self.metadata_clusters[metadata_type]:
+                    if query['func'](value):
+                        yield (url_id, "metadata_{}".format(CONTENT_TYPE_INDEX[metadata_type]), query["string"])
 
     def save_to_file(self, location):
         f = open(location, 'w')
         for data in self:
             f.write('\t'.join(data) + '\n')
         f.close()
-
-
-class MetadataSuggestionsGenerator(object):
-    def __init__(self, stream_contents):
-        self.stream_contents = stream_contents
-        self.clusters_dict = dict()
-
-    def add_cluster(self, metadata_type, cluster_list):
-        if metadata_type not in CONTENT_TYPE_INDEX.values():
-            raise Exception('{} is not a valid metadata type'.format(metadata_type))
-        self.clusters_dict[CONTENT_TYPE_NAME_TO_ID[metadata_type]] = transform_queries(cluster_list, metadata_query_to_python)
-
-    def __iter__(self):
-        for entry in self.stream_contents:
-            url_id, metadata_type, hash_id, value = entry
-            for query in self.clusters_dict[metadata_type]:
-                if query['func'](value):
-                    yield (url_id, "metadata_{}".format(CONTENT_TYPE_INDEX[metadata_type]), query["string"])
--- tests/collections/urls/test_suggestions_generators.py
+++ tests/collections/urls/test_suggestions_generators.py
@@ -4,7 +4,7 @@ import logging
 
 
 from cdf.log import logger
-from cdf.collections.urls.generators.suggestions import UrlSuggestionsGenerator, MetadataSuggestionsGenerator
+from cdf.collections.urls.generators.suggestions import UrlSuggestionsGenerator
 
 logger.setLevel(logging.DEBUG)
 
@@ -23,6 +23,18 @@ class TestUrlSuggestionsGenerator(unittest.TestCase):
             [2, 'http', 'www.site.com', '/another_url.html', '?sid=3JEHG'],
         )
 
+        infos = (
+            [1, 1, 'text/html', 0, 1, 200, 1200, 303, 456],
+            [2, 1, 'text/html', 0, 1, 200, 1200, 303, 456],
+        )
+
+        # 1 = title, 2=h1, 3=H2, 4=Meta desc
+        contents = [
+            [1, 2, 0, 'Teaser : Next episode'],
+            [1, 3, 0, '33 comments about this teaser'],
+            [2, 2, 0, 'Teaser : First episode'],
+        ]
+
         clusters = {
             'path': [
                 'STARTS(path, "/path/")',
@@ -34,31 +46,22 @@ class TestUrlSuggestionsGenerator(unittest.TestCase):
             ]
         }
 
-        u = UrlSuggestionsGenerator(iter(patterns), clusters)
-        results = list(u)
-        self.assertEquals(results[0], (1, 'path', 'STARTS(path, "/path/")'))
-        self.assertEquals(results[1], (1, 'path', 'ENDS(path, ".html")'))
-        self.assertEquals(results[2], (1, 'qs_key', 'query_string = ""'))
-        self.assertEquals(results[3], (2, 'path', 'ENDS(path, ".html")'))
-        self.assertEquals(results[4], (2, 'qs_key', '"sid" in query_string_keys'))
-
-    def test_contents(self):
-        # 1 = title, 2=h1, 3=H2, 4=Meta desc
-        contents = [
-            [1, 2, 0, 'Teaser : Next episode'],
-            [1, 3, 0, '33 comments about this teaser'],
-            [2, 2, 0, 'Teaser : First episode'],
-
-        ]
-
-        u = MetadataSuggestionsGenerator(iter(contents))
-        u.add_cluster('h1', [
+        u = UrlSuggestionsGenerator(iter(patterns), iter(infos), iter(contents))
+        u.add_pattern_cluster('path', clusters['path'])
+        u.add_pattern_cluster('qs_key', clusters['qs_key'])
+        u.add_metadata_cluster('h1', [
             'STARTS(h1, "Teaser :")',
         ])
-        u.add_cluster('h2', [
+        u.add_metadata_cluster('h2', [
             'CONTAINS(h2, " comments about ")',
         ])
         results = list(u)
-        self.assertEquals(results[0], (1, 'metadata_h1', 'STARTS(h1, "Teaser :")'))
-        self.assertEquals(results[1], (1, 'metadata_h2', 'CONTAINS(h2, " comments about ")'))
-        self.assertEquals(results[2], (2, 'metadata_h1', 'STARTS(h1, "Teaser :")'))
+        print results
+        self.assertEquals(results[0], (1, 'path', 'STARTS(path, "/path/")'))
+        self.assertEquals(results[1], (1, 'path', 'ENDS(path, ".html")'))
+        self.assertEquals(results[2], (1, 'qs_key', 'query_string = ""'))
+        self.assertEquals(results[3], (1, 'metadata_h1', 'STARTS(h1, "Teaser :")'))
+        self.assertEquals(results[4], (1, 'metadata_h2', 'CONTAINS(h2, " comments about ")'))
+        self.assertEquals(results[5], (2, 'path', 'ENDS(path, ".html")'))
+        self.assertEquals(results[6], (2, 'qs_key', '"sid" in query_string_keys'))
+        self.assertEquals(results[7], (2, 'metadata_h1', 'STARTS(h1, "Teaser :")'))
