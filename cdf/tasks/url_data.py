9b4684fb560029334477565f6ccd112e199bd08d
ampelmann <thomas@sem.io>
2013-10-17 20:54:51 +0200

    compute metadata duplicates file

--- cdf/tasks/url_data.py
+++ cdf/tasks/url_data.py
@@ -83,6 +83,50 @@ def push_urls_to_elastic_search(crawl_id, part_id, s3_uri, es_location, es_index
         bulk(es, docs, doc_type=es_doc_type, index=es_index)
 
 
+def make_metadata_duplicates_file(crawl_id, s3_uri, first_part_id_size, part_id_size, tmp_dir_prefix='/tmp', force_fetch=False):
+    # Fetch locally the files from S3
+    tmp_dir = os.path.join(tmp_dir_prefix, 'crawl_%d' % crawl_id)
+
+    streams_types = {'patterns': [],
+                     'contents': []
+                     }
+
+    for part_id in xrange(0, nb_parts_from_crawl_location(s3_uri)):
+        files_fetched = fetch_files(s3_uri,
+                                    tmp_dir,
+                                    regexp='url(ids|contents).txt.%d.gz' % part_id,
+                                    force_fetch=force_fetch)
+
+        for path_local, fetched in files_fetched:
+            stream_identifier = STREAMS_FILES[os.path.basename(path_local).split('.')[0]]
+            cast = Caster(STREAMS_HEADERS[stream_identifier.upper()]).cast
+            streams_types[stream_identifier].append(cast(split_file(gzip.open(path_local))))
+
+    generator = get_duplicate_metadata(itertools.chain(*streams_types['patterns']),
+                                       itertools.chain(*streams_types['contents']))
+
+    current_part_id = 0
+    f = gzip.open(os.path.join(tmp_dir, 'urlcontentsduplicate.txt.0.gz'), 'w')
+    for i, (url_id, metadata_type, filled_nb, duplicates_nb, is_first, target_urls_ids) in enumerate(generator):
+
+        # check the part id
+        if (current_part_id == 0 and url_id > first_part_id_size) or \
+           (current_part_id > 0 and (url_id - first_part_id_size) / part_id_size != current_part_id - 1):
+            f.close()
+            current_part_id += 1
+            f = gzip.open(os.path.join(tmp_dir, 'urlcontentsduplicate.txt.{}.gz'.format(current_part_id)), 'w')
+
+        f.write('\t'.join((
+            str(url_id),
+            str(metadata_type),
+            str(filled_nb),
+            str(duplicates_nb),
+            '1' if is_first else '0',
+            ';'.join(str(u) for u in target_urls_ids)
+        )) + '\n')
+    f.close()
+
+
 def push_metadata_duplicate_to_elastic_search(crawl_id, s3_uri, es_location, es_index, es_doc_type, tmp_dir_prefix='/tmp', force_fetch=False):
     # Fetch locally the files from S3
     tmp_dir = os.path.join(tmp_dir_prefix, 'crawl_%d' % crawl_id)
