44027be5a37b020864ebef30307540a0fd5b7d49
Simon Dollé <sdolle@ltutech.com>
2013-11-12 12:43:43 +0100

    remove unused class collections.tagging_stats.aggregator.MetricsAggregator
    remove corresponding tests, and unused functions calling it.

--- cdf/tasks/url_properties.py
+++ cdf/tasks/url_properties.py
@@ -13,7 +13,7 @@ from cdf.streams.mapping import STREAMS_HEADERS, STREAMS_FILES
 from cdf.streams.caster import Caster
 from cdf.streams.utils import split_file, split
 from cdf.collections.urls.generators.tagging import UrlTaggingGenerator
-from cdf.collections.tagging_stats.aggregator import (MetricsAggregator, MetricsConsolidator,
+from cdf.collections.tagging_stats.aggregator import (MetricsConsolidator,
                                                       MetadataAggregator)
 from cdf.utils.s3 import fetch_file, fetch_files, push_content, push_file
 from cdf.utils.es import bulk
@@ -87,45 +87,6 @@ def compute_properties_from_s3(crawl_id, part_id, rev_num, s3_uri, settings, es_
 
     encoded_content = lz4.dumps(content)
     push_content(os.path.join(s3_uri, 'url_properties.rev%d.txt.%d.lz4' % (rev_num, part_id)), encoded_content)
-
-
-def compute_properties_stats_counter_from_s3(crawl_id, part_id, rev_num, s3_uri, tmp_dir_prefix='/tmp', force_fetch=False):
-    # Fetch locally the files from S3
-    tmp_dir = os.path.join(tmp_dir_prefix, 'crawl_%d' % crawl_id)
-    makedirs(tmp_dir, exist_ok=True)
-
-    streams = {}
-
-    properties_file = fetch_file(
-        os.path.join(s3_uri, 'url_properties.rev%d.txt.%d.lz4' % (rev_num, part_id)),
-        os.path.join(tmp_dir, 'url_properties.rev%d.txt.%d.lz4' % (rev_num, part_id)),
-        force_fetch=force_fetch)
-    path_local, fetch = properties_file
-    file_content = lz4.loads(open(path_local).read())
-    if not file_content:
-        return
-    cast = Caster(STREAMS_HEADERS["PROPERTIES"]).cast
-    streams["stream_properties"] = cast(split(file_content.split('\n')))
-
-    files_fetched = fetch_files(s3_uri,
-                                tmp_dir,
-                                regexp=['url(ids|infos|links|inlinks).txt.%d.gz' % part_id],
-                                force_fetch=force_fetch)
-
-    for path_local, fetched in files_fetched:
-        stream_identifier = STREAMS_FILES[os.path.basename(path_local).split('.')[0]]
-        cast = Caster(STREAMS_HEADERS[stream_identifier.upper()]).cast
-        streams["stream_%s" % stream_identifier] = cast(split_file(gzip.open(path_local)))
-
-    # Not crawled urls may be referenced in urlids and urlinfos files, but some files may be missing like inlinks...
-    for optional_stream in ('outlinks', 'inlinks'):
-        stream_key = "stream_{}".format(optional_stream)
-        if not stream_key in streams:
-            streams[stream_key] = iter([])
-
-    aggregator = MetricsAggregator(**streams)
-    content = json.dumps(aggregator.get())
-    push_content(os.path.join(s3_uri, 'properties_stats_partial_rev%d/stats.json.%d' % (rev_num, part_id)), content)
 
 
 def _get_df_properties_stats_counter_from_s3(crawl_id, rev_num, s3_uri, tmp_dir_prefix='/tmp', force_fetch=False):
