1fc078924c0155c7b73e9a6edf95b48ccb67a7d8
Han JU <ju.han.felix@gmail.com>
2013-11-21 11:13:23 +0100

    add a get_part_id helper method that resolves the part_id given a url

--- cdf/collections/urls/utils.py
+++ cdf/collections/urls/utils.py
@@ -9,3 +9,13 @@ def field_has_children(field):
 def children_from_field(field):
     prefix = field + '.'
     return [i for i in QUERY_FIELDS if i[:len(prefix)] == prefix]
+
+
+def get_part_id(url_id, first_part_size, part_size):
+    """Determine which partition a url_id should go into
+    """
+
+    if url_id < first_part_size:
+        return 0
+    else:
+        return (url_id - first_part_size) / part_size + 1
