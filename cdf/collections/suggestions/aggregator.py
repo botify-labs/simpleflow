2fc8d74cf41a661db9ecc796619b3b228bde34b6
Han JU <ju.han.felix@gmail.com>
2013-11-27 16:43:43 +0100

    add total_url and follow_url counters in aggregator/consolidator

--- cdf/collections/suggestions/aggregator.py
+++ cdf/collections/suggestions/aggregator.py
@@ -219,6 +219,14 @@ class MetricsAggregator(object):
             if incanonicals:
                 results[key]['canonical_nb']['incoming'] += incanonicals[incanonical_score_idx]
 
+            if len(inlinks) > 0:
+                results[key]['inlinks_internal_nb']['total_urls'] += 1
+
+            # control flags for `follow_urls` and `total_urls`
+            has_in_follow = False
+            has_out_follow = False
+            has_out_internal = False
+
             # Store metadata counters
             """
             "metadata_nb": {
@@ -278,6 +286,9 @@ class MetricsAggregator(object):
                 results[key][counter_key]['total_unique'] += score_unique
 
                 if follow_key == 'follow':
+                    if not has_in_follow:
+                        has_in_follow = True
+                        results[key][counter_key]['follow_urls'] += 1
                     results[key][counter_key]['follow_unique'] += score_unique
                     inlink_follow_dist(results[key][counter_key], score_unique)
                 else:
@@ -295,10 +306,16 @@ class MetricsAggregator(object):
 
                 if is_internal:
                     results[key][counter_key]['total_unique'] += score_unique
+                    if not has_out_internal:
+                        has_out_internal = True
+                        results[key][counter_key]['total_urls'] += 1
 
                 if follow_key == 'follow':
                     if is_internal:
                         results[key][counter_key]['follow_unique'] += score_unique
+                        if not has_out_follow:
+                            has_out_follow = True
+                            results[key][counter_key]['follow_urls'] += 1
                 else:
                     if follow_key not in results[key][counter_key]['nofollow_combinations']:
                         results[key][counter_key]['nofollow_combinations'][follow_key] = score_unique
