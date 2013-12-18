88d76bb196c7bc8addf3dfba559e31ca19c0741d
ampelmann <thomas@sem.io>
2013-12-18 23:41:26 +0100

    add follow distribution in full_picture json

--- cdf/tasks/aggregators.py
+++ cdf/tasks/aggregators.py
@@ -255,6 +255,7 @@ def make_suggest_summary_file(crawl_id, s3_uri, es_location, es_index, es_doc_ty
             ['global', {}],
             ['http_code', {"group_by": ["http_code"]}],
             ['noindex', {"fields": ["pages_nb"], "group_by": ["index"]}],
+            ['nofollow', {"fields": ["pages_nb"], "group_by": ["follow"]}],
             ['content_type', {"fields": ["pages_nb"], "group_by": ["content_type"]}],
             ['depth', {"fields": ["pages_nb"], "group_by": ["depth"]}],
             ['depth_gt5', {"filters": {"field": "depth", "value": 5, "predicate": "gt"}, "fields": ["pages_nb"]}],
