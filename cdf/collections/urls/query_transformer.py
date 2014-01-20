f6837980c99d895a864598bafcd17ad1486a6d9e
Han JU <ju.han.felix@gmail.com>
2014-01-20 11:19:56 +0100

    simplify query_transformer thanks to newly implemented query parser

--- cdf/collections/urls/query_transformer.py
+++ cdf/collections/urls/query_transformer.py
@@ -1,67 +1,15 @@
 from copy import deepcopy
-from valideer import ValidationError
 
-from cdf.collections.urls.predicate_constants import (DEFAULT_PREDICATE, PREDICATE_FORMATS,
-                                                      BOOL_PREDICATES, NOT_PREDICATE)
-from cdf.collections.urls.query_validator import (validate_boolean_filter, validate_not_filter,
-                                                  validate_predicate_filter, validate_fields,
-                                                  validate_sorts)
-from cdf.exceptions import BotifyQueryException
+from cdf.collections.urls.query_parsing import parse_botify_query
 
 
 __ALL__ = ['get_es_query']
-
-_DEFAULT_SORT = [{'id': {'ignore_unmapped': True}}]
-_DEFAULT_FIELD = ['url']
-
-
-def _validate(validate_func, elem):
-    """Wraps validation function for better exception message"""
-    try:
-        validate_func(elem)
-    except ValidationError as err:
-        raise BotifyQueryException(
-            "Botify query validation error: {} in {}".format(
-                err.message, str(elem)))
-
-
-def _is_boolean_filter(filter_dict):
-    return isinstance(filter_dict, dict) and \
-           len(filter_dict) == 1 and \
-           filter_dict.keys()[0].lower() in BOOL_PREDICATES
-
-
-def _is_not_filter(filter_dict):
-    return isinstance(filter_dict, dict) and \
-           len(filter_dict) == 1 and \
-           filter_dict.keys()[0].lower() == NOT_PREDICATE
-
-
-def _parse_predicate_filter(predicate_filter):
-    predicate = predicate_filter.get('predicate', DEFAULT_PREDICATE)
-    return PREDICATE_FORMATS[predicate](predicate_filter)
-
-
-def _parse_filter(filter):
-    # boolean filter
-    if _is_boolean_filter(filter):
-        _validate(validate_boolean_filter, filter)
-        operator, filters = filter.items()[0]
-        return {operator: [_parse_filter(f) for f in filters]}
-    elif _is_not_filter(filter):
-        _validate(validate_not_filter, filter)
-        _, other_filter = filter.items()[0]
-        return {NOT_PREDICATE: _parse_filter(other_filter)}
-    # predicate filter
-    else:
-        _validate(validate_predicate_filter, filter)
-        return _parse_predicate_filter(filter)
 
 
 def _merge_filters(query, filters):
     """Merge filters to botify query using `and` filter
 
-    New filters are places before the original filters.
+    New filters are places BEFORE the original filters.
 
     :param query: the botify format query
     :param filters: a list of botify predicate to merge
@@ -85,55 +33,6 @@ def _merge_filters(query, filters):
     return botify_query
 
 
-def _parse_sorts(sorts):
-    """Process sort options and add default sort parameters
-
-    :param sorts: a list of sort options of botify_query format,
-        eg. ['url', {'depth': {'order': 'desc'}}]
-    :returns: the corresponding ElasticSearch query sort component
-    """
-    _validate(validate_sorts, sorts)
-    es_sorts = []
-    for sort in sorts:
-        if isinstance(sort, dict):
-            # sort contains order option
-            field = list(sort.keys())[0]
-            es_sorts.append({
-                field: {
-                    'ignore_unmapped': True,
-                    'order': sort[field]['order']
-                }
-            })
-        elif isinstance(sort, str):
-            es_sorts.append({
-                sort: {
-                    'ignore_unmapped': True,
-                }
-            })
-    return es_sorts
-
-
-def _parse_fields(fields):
-    _validate(validate_fields, fields)
-    return fields
-
-
-def _wrap_query(unwrapped):
-    """Wrap a processed botify query into its final form
-
-    Currently a `constant_score` query is used
-
-    :param unwrapped: processed botify query containing
-        `fields`, `filter` and `sort`
-    """
-    filters = {'filter': unwrapped['filter']}
-    return {
-        'query': {'constant_score': filters},
-        'sort': unwrapped['sort'],
-        'fields': unwrapped['fields']
-    }
-
-
 def get_es_query(botify_query, crawl_id):
     """Generate ElasticSearch query from a botify query
 
@@ -141,7 +40,6 @@ def get_es_query(botify_query, crawl_id):
     :param crawl_id: unique id of the crawl in question
     :returns: a valid ElasticSearch query, in json format
     """
-    # TODO add a validator for global format validation
 
     # By default all queries should have these filter/predicate
     #   1. only query for current crawl/site
@@ -156,23 +54,4 @@ def get_es_query(botify_query, crawl_id):
     # Merge default filters in botify format query
     botify_query = _merge_filters(botify_query, default_filters)
 
-    # Transform botify query to ElasticSearch query
-    es_query = {}
-
-    if 'sort' in botify_query:
-        es_query['sort'] = _parse_sorts(botify_query['sort'])
-    else:
-        es_query['sort'] = _DEFAULT_SORT
-
-    if 'filters' in botify_query:
-        es_query['filter'] = _parse_filter(botify_query['filters'])
-    else:
-        raise BotifyQueryException(
-            'No filter component in query: {}'.format(botify_query))
-
-    if 'fields' in botify_query:
-        es_query['fields'] = _parse_fields(botify_query['fields'])
-    else:
-        es_query['fields'] = _DEFAULT_FIELD
-
-    return _wrap_query(es_query)
+    return parse_botify_query(botify_query).transform()
