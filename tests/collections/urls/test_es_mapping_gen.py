f7dfa023d8029891bba61b72d46413d5759b79e6
Han JU <ju.han.felix@gmail.com>
2014-01-21 14:16:14 +0100

    move old, hard-coded ES mapping to unit test part

--- tests/collections/urls/test_es_mapping_gen.py
+++ tests/collections/urls/test_es_mapping_gen.py
@@ -3,8 +3,7 @@ from cdf.collections.urls.es_mapping_generation import (_parse_field_path,
                                                         generate_es_mapping,
                                                         generate_default_value_lookup,
                                                         generate_valid_field_lookup)
-from cdf.collections.urls.constants import (URLS_DATA_MAPPING_DEPRECATED,
-                                            URLS_DATA_FORMAT_DEFINITION)
+from cdf.collections.urls.constants import URLS_DATA_FORMAT_DEFINITION
 
 
 class TestMappingGeneration(unittest.TestCase):
@@ -176,3 +175,225 @@ class TestMappingGeneration(unittest.TestCase):
         }
 
         self.assertEqual(result, expected)
+
+URLS_DATA_MAPPING_DEPRECATED = {
+    "urls": {
+        "_routing": {
+            "required": True,
+            "path": "crawl_id"
+        },
+        "properties": {
+            "url": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "url_hash": {"type": "long"},
+            "byte_size": {"type": "long"},
+            "date_crawled": {"type": "date"},
+            "delay1": {"type": "long"},
+            "delay2": {"type": "long"},
+            "depth": {"type": "long"},
+            "gzipped": {"type": "boolean"},
+            "host": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "http_code": {"type": "long"},
+            "id": {"type": "long"},
+            "crawl_id": {"type": "long"},
+            "patterns": {"type": "long"},
+            "metadata_nb": {
+                "properties": {
+                    "description": {"type": "long"},
+                    "h1": {"type": "long"},
+                    "h2": {"type": "long"},
+                    "title": {"type": "long"}
+                }
+            },
+            "metadata": {
+                "properties": {
+                    "description": {
+                        "type": "multi_field",
+                        "fields": {
+                            "description": {
+                                "type": "string"
+                            },
+                            "untouched": {
+                                "type": "string",
+                                "index": "not_analyzed"
+                            }
+                        }
+                    },
+                    "h1": {
+                        "type": "multi_field",
+                        "fields": {
+                            "h1": {
+                                "type": "string"
+                            },
+                            "untouched": {
+                                "type": "string",
+                                "index": "not_analyzed"
+                            }
+                        }
+                    },
+                    "h2": {
+                        "type": "multi_field",
+                        "fields": {
+                            "h2": {
+                                "type": "string"
+                            },
+                            "untouched": {
+                                "type": "string",
+                                "index": "not_analyzed"
+                            }
+                        }
+                    },
+                    "title": {
+                        "type": "multi_field",
+                        "fields": {
+                            "title": {
+                                "type": "string"
+                            },
+                            "untouched": {
+                                "type": "string",
+                                "index": "not_analyzed"
+                            }
+                        }
+                    }
+                }
+            },
+            "metadata_duplicate_nb": {
+                "properties": {
+                    "title": {"type": "long"},
+                    "description": {"type": "long"},
+                    "h1": {"type": "long"}
+                }
+            },
+            "metadata_duplicate": {
+                "properties": {
+                    "title": {"type": "long", "index": "no"},
+                    "description": {"type": "long", "index": "no"},
+                    "h1": {"type": "long", "index": "no"}
+                }
+            },
+            "metadata_duplicate_is_first": {
+                "properties": {
+                    "title": {"type": "boolean"},
+                    "description": {"type": "boolean"},
+                    "h1": {"type": "boolean"}
+                }
+            },
+            "inlinks_internal_nb": {
+                "properties": {
+                    "total": {"type": "long"},
+                    "follow_unique": {"type": "long"},
+                    "total_unique": {"type": "long"},
+                    "follow": {"type": "long"},
+                    "nofollow": {"type": "long"},
+                    "nofollow_combinations": {
+                        "properties": {
+                            "key": {"type": "string"},
+                            "value": {"type": "long"}
+                        }
+                    }
+                }
+            },
+            "inlinks_internal": {"type": "long", "index": "no"},
+            "outlinks_internal": {"type": "long", "index": "no"},
+            "outlinks_internal_nb": {
+                "properties": {
+                    "total": {"type": "long"},
+                    "follow_unique": {"type": "long"},
+                    "total_unique": {"type": "long"},
+                    "follow": {"type": "long"},
+                    "nofollow": {"type": "long"},
+                    "nofollow_combinations": {
+                        "properties": {
+                            "key": {"type": "string"},
+                            "value": {"type": "long"}
+                        }
+                    }
+                }
+            },
+            "outlinks_external_nb": {
+                "properties": {
+                    "total": {"type": "long"},
+                    "follow": {"type": "long"},
+                    "nofollow": {"type": "long"},
+                    "nofollow_combinations": {
+                        "properties": {
+                            "key": {"type": "string"},
+                            "value": {"type": "long"}
+                        }
+                    }
+                }
+            },
+            "path": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "protocol": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "query_string": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "query_string_keys": {
+                "type": "string",
+                "index": "not_analyzed"
+            },
+            "canonical_from_nb": {"type": "long"},
+            "canonical_from": {"type": "long", "index": "no"},
+            "canonical_to": {
+                "properties": {
+                    "url": {"type": "string", "index": "no"},
+                    "url_id": {"type": "long", "index": "no"}
+                }
+            },
+            "canonical_to_equal": {"type": "boolean"},
+            "redirects_to": {
+                "properties": {
+                    "http_code": {"type": "long"},
+                    "url": {"type": "string"},
+                    "url_id": {"type": "long"}
+                }
+            },
+            "redirects_from_nb": {"type": "long"},
+            "redirects_from": {
+                "properties": {
+                    "http_code": {"type": "long", "index": "no"},
+                    "url_id": {"type": "long", "index": "no"}
+                }
+            },
+            "error_links": {
+                "properties": {
+                    "3xx": {
+                        "properties": {
+                            "nb": {"type": "long"},
+                            "urls": {"type": "long", "index": "no"}
+                        }
+                    },
+                    "4xx": {
+                        "properties": {
+                            "nb": {"type": "long"},
+                            "urls": {"type": "long", "index": "no"}
+                        }
+                    },
+                    "5xx": {
+                        "properties": {
+                            "nb": {"type": "long"},
+                            "urls": {"type": "long", "index": "no"}
+                        }
+                    },
+                    "any": {
+                        "properties": {
+                            "nb": {"type": "long"}
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
