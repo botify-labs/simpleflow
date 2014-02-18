a97dff5cb9f8b8da01f516f916a9bf37bd678426
Greg Leclercq <greg@sem.io>
2014-02-18 22:50:49 +0100

    Add class futures.Future

---
+++ tests/futures/test_base.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from simpleflow.futures import Future, base
+
+
+def test_future_init_state():
+    assert Future()._state == base.PENDING
+
+
+def test_future_init_result():
+    assert Future()._result is None
+
+
+def test_future_init_repr():
+    future = Future()
+    assert repr(future) == '<Future at {} state=pending>'.format(
+        hex(id(future)))
+
+
+def test_future_init_cancelled():
+    assert Future().cancelled() is False
+
+
+def test_future_init_running():
+    assert Future().running() is False
+
+
+def test_future_init_done():
+    assert Future().done() is False
