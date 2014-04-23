a4a4e4f45b91474f890eddc7ffd27fb0d9f61acb
ampelmann <thomas@sem.io>
2014-04-23 16:39:51 +0200

    prepare analytics data to be pushed in ES document

--- cdf/features/ganalytics/streams.py
+++ cdf/features/ganalytics/streams.py
@@ -1,3 +1,6 @@
+from cdf.metadata.url.url_metadata import (
+    INT_TYPE, ES_DOC_VALUE, AGG_NUMERICAL
+)
 from cdf.core.features import StreamDefBase
 
 
@@ -14,11 +17,42 @@ class RowVisitsStreamDef(StreamDefBase):
 class VisitsStreamDef(StreamDefBase):
     FILE = 'analytics_data'
     HEADERS = (
-        ('url_id', int),
+        ('id', int),
         ('medium', str),
         ('source', str),
         ('nb_visits', int),
     )
+    URLS_DOCUMENT_MAPPING = {
+        "visits.organic.google.nb": {
+            "type": INT_TYPE,
+            "settings": {
+                ES_DOC_VALUE,
+                AGG_NUMERICAL
+            }
+        },
+        "visits.organic.bing.nb": {
+            "type": INT_TYPE,
+            "settings": {
+                ES_DOC_VALUE,
+                AGG_NUMERICAL
+            }
+        }
+    }
 
-    def process_document(self):
-        pass
+    def pre_process_document(self, document):
+        document["visits"] = {
+            "organic": {
+                "google": {
+                    "nb": 0
+                },
+                "bing": {
+                    "nb": 0
+                }
+            }
+        }
+
+    def process_document(self, document, stream):
+        _, medium, source, nb_visits = stream
+        if medium != 'organic' or source not in ('google', 'bing'):
+            return
+        document['visits'][medium][source]['nb'] = nb_visits
