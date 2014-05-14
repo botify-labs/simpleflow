5471c220fbbd3f67f2bed3bdeea9d89e1bf59fc2
Simon Dollé <simon@botify.com>
2014-05-14 11:10:09 +0200

    reduce number of search engines and social networks
    keep only google, bing, yahoo, facebook and twitter
    
    We remove some traffic sources to reduce clutter in URL Explorer.

--- cdf/features/ganalytics/settings.py
+++ cdf/features/ganalytics/settings.py
@@ -2,19 +2,9 @@ ORGANIC_SOURCES = (
     'google',
     'bing',
     'yahoo',
-    "ask",
-    "aol",
-    "yandex",
-    "baidu",
-    "naver"
 )
 
 SOCIAL_SOURCES = (
     'facebook',
     'twitter',
-    'pinterest',
-    'linkedin',
-    'reddit',
-    'google+',
-    'tumblr'
 )
