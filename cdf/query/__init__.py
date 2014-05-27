43feaafc48d7b42f9fc9dd92662501e2c391749a
ampelmann <thomas@sem.io>
2014-05-27 12:26:37 +0200

    remove imports into cdf/query/__init__.py

--- cdf/query/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Abstraction layer between front-end's query and backend storage
-"""
-
-from query import Query as UrlQuery
-from cdf.analysis.suggestions.query import MetricsQuery, SuggestSummaryQuery
