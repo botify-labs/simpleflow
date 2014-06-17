5d739e8248ceed333cbc070dcd0ad83543cebbec
Simon Dollé <simon@botify.com>
2014-06-17 19:07:44 +0200

    Create download_sitemap_file and download_sitemap_files
    download_sitemap_file is not used as a task by is ready for it.

--- cdf/features/sitemap/tasks.py
+++ cdf/features/sitemap/tasks.py
@@ -21,10 +21,34 @@ def download_sitemap_files(input_urls, s3_uri, tmp_dir=None, force_fetch=False):
     :param tmp_dir: the path to the directory where to save the files
     :type tmp_dir: str
     """
-    file_index = {}
+    s3_file_index = {}
     for url in input_urls:
-        crt_file_index = download_sitemaps(url, tmp_dir)
-        file_index.update(crt_file_index)
+        crt_file_index = download_sitemap_file(url, s3_uri, tmp_dir, force_fetch)
+        s3_file_index.update(crt_file_index)
+
+    s3_subdir_uri = os.path.join(s3_uri, "sitemaps")
+
+    #push the file that list the sitemap files
+    s3.push_content(
+        os.path.join(s3_subdir_uri, "file_index.json"),
+        json.dumps(s3_file_index)
+    )
+
+
+def download_sitemap_file(input_url, s3_uri, tmp_dir=None, force_fetch=False):
+    """Download all sitemap files related to an input url and upload them to s3.
+    If the input url is a sitemap, the file will simply be downloaded,
+    if it is a sitemap index, it will download the listed sitemaps
+    The function returns a dict original url -> s3 uri.
+    :param input_urls: a list of sitemap/sitemap index urls
+    :type input_urls: list
+    :param s3_uri: the s3 uri where the crawl data is stored.
+    :type s3_uri: str
+    :param tmp_dir: the path to the directory where to save the files
+    :type tmp_dir: str
+    :returns: dict
+    """
+    file_index = download_sitemaps(input_url, tmp_dir)
 
     s3_subdir_uri = os.path.join(s3_uri, "sitemaps")
     #a dict similar to file_locations but that stores s3 uris
@@ -37,8 +61,4 @@ def download_sitemap_files(input_urls, s3_uri, tmp_dir=None, force_fetch=False):
         )
         s3_file_index[url] = destination_uri
 
-    #push the file that list the sitemap files
-    s3.push_content(
-        os.path.join(s3_subdir_uri, "file_index.json"),
-        json.dumps(s3_file_index)
-    )
+    return s3_file_index
--- tests/features/sitemap/test_task.py
+++ tests/features/sitemap/test_task.py
@@ -2,21 +2,20 @@ import unittest
 import mock
 import json
 
-from cdf.features.sitemap.tasks import download_sitemap_files
+from cdf.features.sitemap.tasks import (download_sitemap_files,
+                                        download_sitemap_file)
 
 
 class TestDownloadSitemapFiles(unittest.TestCase):
     @mock.patch('cdf.utils.s3.push_content',)
-    @mock.patch('cdf.utils.s3.push_file')
-    @mock.patch('cdf.features.sitemap.tasks.download_sitemaps')
+    @mock.patch('cdf.features.sitemap.tasks.download_sitemap_file')
     def test_nominal_case(self,
-                          download_sitemap_files_mock,
-                          push_file_mock,
+                          download_sitemap_file_mock,
                           push_content_mock):
         #mocking
-        download_sitemap_files_mock.side_effect = [
-            {"http://foo.com/sitemap.xml": "/tmp/foo/sitemap.xml"},
-            {"http://bar.com/sitemap.xml": "/tmp/foo/sitemap.xml_2"},
+        download_sitemap_file_mock.side_effect = [
+            {"http://foo.com/sitemap.xml": "s3://foo/sitemaps/sitemap.xml"},
+            {"http://bar.com/sitemap.xml": "s3://foo/sitemaps/sitemap.xml_2"},
         ]
 
         #actual call
@@ -28,15 +27,39 @@ class TestDownloadSitemapFiles(unittest.TestCase):
         download_sitemap_files(input_urls, s3_uri)
 
         #verifications
-        self.assertItemsEqual(
-            [mock.call("s3://foo/sitemaps/sitemap.xml", "/tmp/foo/sitemap.xml"),
-             mock.call("s3://foo/sitemaps/sitemap.xml_2", "/tmp/foo/sitemap.xml_2")],
-            push_file_mock.mock_calls)
-
         expected_file_index = {
             "http://foo.com/sitemap.xml": "s3://foo/sitemaps/sitemap.xml",
             "http://bar.com/sitemap.xml": "s3://foo/sitemaps/sitemap.xml_2",
         }
         push_content_mock.assert_called_once_with(
-            "s3://foo/sitemaps/file_index.json", json.dumps(expected_file_index)
+            "s3://foo/sitemaps/file_index.json",
+            json.dumps(expected_file_index)
+        )
+
+
+class TestDownloadSitemapFile(unittest.TestCase):
+    @mock.patch('cdf.utils.s3.push_file')
+    @mock.patch('cdf.features.sitemap.tasks.download_sitemaps')
+    def test_nominal_case(self,
+                          download_sitemaps_mock,
+                          push_file_mock):
+        #mocking
+        download_sitemaps_mock.return_value = {
+            "http://foo.com/sitemap.xml": "/tmp/foo/sitemap.xml"
+        }
+
+        #actual call
+        input_url = "http://foo.com/sitemap.xml"
+        s3_uri = "s3://foo"
+        actual_result = download_sitemap_file(input_url, s3_uri)
+
+        #verifications
+        expected_result = {
+            "http://foo.com/sitemap.xml": "s3://foo/sitemaps/sitemap.xml",
+        }
+        self.assertEqual(expected_result, actual_result)
+
+        push_file_mock.assert_called_once_with(
+            "s3://foo/sitemaps/sitemap.xml",
+            "/tmp/foo/sitemap.xml"
         )
