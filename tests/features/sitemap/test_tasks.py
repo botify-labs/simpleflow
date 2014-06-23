c29d557efd9eb6df92f11e6f535f3d1b4e6dc3ef
Simon Dollé <simon@botify.com>
2014-06-23 15:52:46 +0200

    Add unit test match_sitemap_urls()

--- tests/features/sitemap/test_tasks.py
+++ tests/features/sitemap/test_tasks.py
@@ -1,11 +1,15 @@
 import unittest
 import mock
+import tempfile
+import gzip
+import os
 
 from cdf.features.sitemap.download import Sitemap, DownloadStatus
-from cdf.core.streams.base import TemporaryDataset
+from cdf.features.main.streams import IdStreamDef
 from cdf.features.sitemap.tasks import (download_sitemap_files,
-                                        download_sitemap_file)
-from cdf.features.sitemap.matching import match_sitemap_urls_from_stream
+                                        download_sitemap_file,
+                                        match_sitemap_urls)
+from cdf.core.mocks import _mock_push_file
 
 
 class TestDownloadSitemapFiles(unittest.TestCase):
@@ -75,3 +79,42 @@ class TestDownloadSitemapFile(unittest.TestCase):
             "s3://foo/sitemaps/sitemap.xml",
             "/tmp/foo/sitemap.xml"
         )
+
+
+class TestMatchSitemapUrls(unittest.TestCase):
+    @mock.patch('cdf.utils.s3.push_file', _mock_push_file)
+    @mock.patch("cdf.features.sitemap.tasks.get_sitemap_urls_stream", autospec=True)
+    @mock.patch.object(IdStreamDef, 'get_stream_from_s3')
+    def test_nominal_case(self,
+                          get_stream_from_s3_mock,
+                          get_sitemap_urls_stream_mock):
+        #mock definition
+        get_stream_from_s3_mock.return_value = [
+            (1, "http", "foo.com", "/bar", ""),
+            (2, "http", "foo.com", "/baz", ""),
+            (3, "http", "foo.com", "/qux", ""),
+        ]
+
+        get_sitemap_urls_stream_mock.return_value = [
+            "http://foo.com/qux",
+            "http://foo.com/bar",
+            "http://foo.com/index.html"  # not in crawl
+        ]
+
+        #call
+        s3_uri = "s3://" + tempfile.mkdtemp()
+        first_part_id_size = 10
+        part_id_size = 100
+
+        match_sitemap_urls(s3_uri,
+                           first_part_id_size,
+                           part_id_size)
+
+        #check output files
+        with gzip.open(os.path.join(s3_uri[5:], 'sitemap_only.gz')) as f:
+            expected_result = ['http://foo.com/index.html\n']
+            self.assertEquals(expected_result, f.readlines())
+
+        with gzip.open(os.path.join(s3_uri[5:], 'sitemap.txt.0.gz')) as f:
+            expected_result = ['1\n', '3\n']  # urlids are now sorted
+            self.assertEquals(expected_result, f.readlines())
