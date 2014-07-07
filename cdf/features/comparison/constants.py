9e5b511326ac7d27397aebb748bde5d361000404
Han JU <ju.han.felix@gmail.com>
2014-07-07 11:57:49 +0200

    fix typo

--- cdf/features/comparison/constants.py
+++ cdf/features/comparison/constants.py
@@ -1,6 +1,6 @@
 # Separator for encoding the url_id with the url string
 # 2 conditions:
-#   - not a valida char in url string
+#   - not a valid char in url string
 #   - does not alter the url string ordering
 # with above in mind, `\0` seems to be the best choice
 SEPARATOR = '\0'
