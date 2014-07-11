fa469dfb605fdac9c1e9f98b19d561045c5d721a
Han JU <ju.han.felix@gmail.com>
2014-07-11 18:07:15 +0200

    make system wide libs visible inside jenkinks test venv

--- tests/jenkins.sh
+++ tests/jenkins.sh
@@ -9,7 +9,7 @@ SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $SCRIPT_DIR/.. #back to root directory
 
 VIRTUALENV_DIR="/tmp/venv"
-virtualenv $VIRTUALENV_DIR
+virtualenv $VIRTUALENV_DIR --system-site-packages
 [ $? -ne 0 ] && exit 1
 source $VIRTUALENV_DIR/bin/activate
 
