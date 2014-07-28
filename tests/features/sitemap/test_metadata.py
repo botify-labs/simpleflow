591d9ba093d150ff02dd2b01bd2d3f5435037135
Simon Dollé <simon@botify.com>
2014-07-28 10:23:27 +0200

    Add unittests for SitemapMetadata.to_dict()

--- tests/features/sitemap/test_metadata.py
+++ tests/features/sitemap/test_metadata.py
@@ -7,6 +7,72 @@ from cdf.features.sitemap.metadata import (Metadata,
 from cdf.features.sitemap.document import SiteMapType
 
 
+class TestSitemapMetadata(unittest.TestCase):
+    def setUp(self):
+        self.url = "http://foo.com/sitemap.xml"
+        self.s3_uri = "s3://foo.com/sitemap.xml"
+        self.sitemap_index = "http://foo.com/sitemap_index.xml"
+
+        self.error_type = "ParsingError"
+        self.error_message = "error message"
+
+        self.valid_urls = 10
+        self.invalid_urls = 5
+
+    def test_to_dict_nominal_case(self):
+        sitemap_metadata = SitemapMetadata(self.url,
+                                           self.s3_uri,
+                                           self.sitemap_index)
+
+        expected_result = {
+            "url": self.url,
+            "s3_uri": self.s3_uri,
+            "sitemap_index": self.sitemap_index
+        }
+
+        self.assertEqual(expected_result, sitemap_metadata.to_dict())
+
+    def test_to_dict_no_sitemap_index(self):
+        sitemap_metadata = SitemapMetadata(self.url,
+                                           self.s3_uri)
+
+        expected_result = {
+            "url": self.url,
+            "s3_uri": self.s3_uri,
+        }
+
+        self.assertEqual(expected_result, sitemap_metadata.to_dict())
+
+    def test_to_dict_error_case(self):
+        sitemap_metadata = SitemapMetadata(self.url,
+                                           self.s3_uri)
+        sitemap_metadata.error_type = self.error_type
+        sitemap_metadata.error_message = self.error_message
+        expected_result = {
+            "url": self.url,
+            "s3_uri": self.s3_uri,
+            "error": self.error_type,
+            "message": self.error_message
+        }
+
+        self.assertEqual(expected_result, sitemap_metadata.to_dict())
+
+    def test_to_dict_valid_invalid_urls(self):
+        sitemap_metadata = SitemapMetadata(self.url,
+                                           self.s3_uri)
+        sitemap_metadata.valid_urls = self.valid_urls
+        sitemap_metadata.invalid_urls = self.invalid_urls
+
+        expected_result = {
+            "url": self.url,
+            "s3_uri": self.s3_uri,
+            "valid_urls": self.valid_urls,
+            "invalid_urls": self.invalid_urls
+        }
+
+        self.assertEqual(expected_result, sitemap_metadata.to_dict())
+
+
 class TestMetadata(unittest.TestCase):
     def setUp(self):
         self.sitemap_index = "http://foo/sitemap_index.xml"
