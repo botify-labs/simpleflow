c6bca2c00263ee29912fd5243c41da3915cdf66a
Simon Dollé <simon@botify.com>
2014-08-12 12:30:32 +0200

    Define insights for "semantic_metadata" feature.

---
+++ cdf/features/semantic_metadata/insights.py
@@ -0,0 +1,54 @@
+from cdf.core.insights import Insight
+from cdf.query.filter import EqFilter, GtFilter
+
+
+def get_metadata_insights(metadata):
+    """Return the list of insights for a given metadata: title, description or h1
+    :param metadata: the input metadata
+    :type metadata: str
+    :returns: list - the list of corresponding insights_module_name
+    """
+    result = []
+
+    #unique
+    identifier = "meta:{}:unique".format(metadata)
+    name = "Unique {}".format(metadata.title())
+    field = "metadata.{}.duplicates.nb".format(metadata)
+    result.append(
+        Insight(
+            identifier,
+            name,
+            EqFilter(field, 0).to_dict()
+        )
+    )
+
+    #not set
+    identifier = "meta:{}:not_set".format(metadata)
+    name = "Not Set {}".format(metadata.title())
+    field = "metadata.{}.nb".format(metadata)
+    result.append(
+        Insight(
+            identifier,
+            name,
+            EqFilter(field, 0).to_dict()
+        )
+    )
+
+    #duplicate
+    identifier = "meta:{}:duplicate".format(metadata)
+    name = "Duplicate {}".format(metadata.title())
+    field = "metadata.{}.duplicates.nb".format(metadata)
+    result.append(
+        Insight(
+            identifier,
+            name,
+            GtFilter(field, 0).to_dict()
+        )
+    )
+
+    return result
+
+#actual insight definition
+insights = []
+for metadata in ["title", "description", "h1"]:
+    insights.extend(get_metadata_insights(metadata))
