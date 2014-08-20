5c3123359fe6a039c5559b78f9e83eb5f66c8de6
Simon Dollé <simon@botify.com>
2014-08-20 11:36:11 +0200

    Add unittest for compute_zones()

--- tests/features/main/test_tasks.py
+++ tests/features/main/test_tasks.py
@@ -1,6 +1,12 @@
 import unittest
+from moto import mock_s3
+import boto
+import tempfile
+import shutil
+import gzip
 
-from cdf.features.main.tasks import get_lang
+from cdf.features.main.tasks import get_lang, compute_zones
+from cdf.features.main.streams import ZoneStreamDef
 
 
 class TestGetLang(unittest.TestCase):
@@ -15,3 +21,59 @@ class TestGetLang(unittest.TestCase):
 
     def test_lang_unknown(self):
         self.assertEqual("undef", get_lang(self.info_entry, 3))
+
+
+class TestComputeZones(unittest.TestCase):
+    @mock_s3
+    def test_nominal_case(self):
+
+        bucket = "app.foo.com"
+        s3_uri = "s3://{}/crawl_result".format(bucket)
+        conn = boto.connect_s3()
+        bucket = conn.create_bucket(bucket)
+
+        #create urlids
+        urlids = boto.s3.key.Key(bucket)
+        urlids.key = "crawl_result/urlids.txt.0.gz"
+        f = tempfile.NamedTemporaryFile(delete=False)
+        f.close()
+        with gzip.open(f.name, "w") as tmp_file:
+            tmp_file.write("1\thttp\tfoo.com\t/\n")
+            tmp_file.write("2\thttps\tfoo.com\t/bar\n")
+            tmp_file.write("9\thttps\tfoo.com\t/baz\n")
+        urlids.set_contents_from_filename(f.name)
+        os.remove(f.name)
+
+        #create urlinfos
+        urlinfos = boto.s3.key.Key(bucket)
+        urlinfos.key = "crawl_result/urlinfos.txt.0.gz"
+        bulk_data = "48\ttext/html\t0\t7695895\t200\t51133\t203\t305"
+        f = tempfile.NamedTemporaryFile(delete=False)
+        f.close()
+        with gzip.open(f.name, "w") as tmp_file:
+            tmp_file.write("1\t{}\ten-US\n".format(bulk_data))
+            tmp_file.write("2\t{}\tfr\n".format(bulk_data))
+            tmp_file.write("9\t{}\tfr\n".format(bulk_data))
+        urlinfos.set_contents_from_filename(f.name)
+        os.remove(f.name)
+
+        #actual computation
+        part_id = 0
+        document_uri = compute_zones(s3_uri,
+                                     part_id)
+
+        #check output
+        expected_document_uri = "{}/zones.txt.{}.gz".format(s3_uri, part_id)
+        self.assertEqual(expected_document_uri, document_uri)
+
+        tmp_dir = tempfile.mkdtemp()
+        zone_stream = ZoneStreamDef.get_stream_from_s3(s3_uri,
+                                                       tmp_dir=tmp_dir,
+                                                       part_id=part_id)
+        expected_zone_stream = [
+            [1, 'en-US,http'],
+            [2, 'fr,https'],
+            [9, 'fr,https']
+        ]
+        self.assertEqual(expected_zone_stream, list(zone_stream))
+        shutil.rmtree(tmp_dir)
