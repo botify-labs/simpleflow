0d273b659359ee26473e34dccfcfd3a63ed09c2c
Simon Dollé <simon@botify.com>
2014-08-25 10:01:07 +0200

    Use get_stream_from_s3() and persist_stream_to_s3() in make_metadata_duplicates_file()
    get_stream_from_s3() returns a Stream.
    This fixes the bug.
    
    Using persist_stream_to_s3() also makes the task much easier to read.

--- cdf/features/semantic_metadata/tasks.py
+++ cdf/features/semantic_metadata/tasks.py
@@ -1,17 +1,12 @@
-import os
-import gzip
 import itertools
-
 from cdf.log import logger
-from cdf.utils.path import write_by_part
-from cdf.utils.s3 import push_file
 from cdf.analysis.urls.transducers.metadata_duplicate import (
     get_duplicate_metadata,
     count_metadata
 )
-from cdf.utils.remote_files import nb_parts_from_crawl_location
 from cdf.features.semantic_metadata.streams import (
     ContentsStreamDef,
+    ContentsDuplicateStreamDef,
     ContentsCountStreamDef
 )
 from cdf.tasks.decorators import TemporaryDirTask as with_temporary_dir
@@ -32,50 +27,37 @@ def compute_metadata_count(s3_uri, part_id, tmp_dir=None):
         s3_uri,
         part_id=part_id
     )
+
     return s3_destination
+
+
+def to_string(row):
+    """Format a row from get_duplicate_metadata() so that
+    it can be processed by persist_to_s3()
+    It transforms the booleans in integer
+    and the lists in strings.
+    """
+    url_id, metadata_type, duplicates_nb, is_first, target_urls_ids = row
+    return (url_id,
+            metadata_type,
+            duplicates_nb,
+            1 if is_first else 0,
+            ';'.join(str(u) for u in target_urls_ids)
+           )
 
 
 @with_temporary_dir
 def make_metadata_duplicates_file(crawl_id, s3_uri,
                                   first_part_id_size, part_id_size,
-                                  tmp_dir=None, force_fetch=DEFAULT_FORCE_FETCH):
-    def to_string(row):
-        url_id, metadata_type, duplicates_nb, is_first, target_urls_ids = row
-        return '\t'.join((
-            str(url_id),
-            str(metadata_type),
-            str(duplicates_nb),
-            '1' if is_first else '0',
-            ';'.join(str(u) for u in target_urls_ids)
-        )) + '\n'
-
-    nb_parts = nb_parts_from_crawl_location(s3_uri)
-
-    logger.info('Fetching all partitions from S3')
-    streams = []
-    for part_id in xrange(0, nb_parts):
-        streams.append(
-            ContentsStreamDef.get_stream_from_s3(
-                s3_uri,
-                tmp_dir=tmp_dir,
-                part_id=part_id,
-                force_fetch=force_fetch
-            )
-        )
-
-    generator = get_duplicate_metadata(itertools.chain(*streams))
-
-    file_pattern = 'urlcontentsduplicate.txt.{}.gz'
-    write_by_part(generator, first_part_id_size, part_id_size,
-                  tmp_dir, file_pattern, to_string)
-
-    # push all created files to s3
-    logger.info('Pushing metadata duplicate file to s3')
-    for i in xrange(0, nb_parts + 1):
-        file_to_push = file_pattern.format(i)
-        if os.path.exists(os.path.join(tmp_dir, file_to_push)):
-            logger.info('Pushing {}'.format(file_to_push))
-            push_file(
-                os.path.join(s3_uri, file_to_push),
-                os.path.join(tmp_dir, file_to_push),
-            )
+                                  tmp_dir=None,
+                                  force_fetch=DEFAULT_FORCE_FETCH):
+    logger.info('Fetching contents stream from S3')
+    contents_stream = ContentsStreamDef.get_stream_from_s3(s3_uri,
+                                                           tmp_dir=tmp_dir)
+    generator = get_duplicate_metadata(contents_stream)
+    generator = itertools.imap(to_string, generator)
+    files = ContentsDuplicateStreamDef.persist_to_s3(generator,
+                                                     s3_uri,
+                                                     first_part_id_size,
+                                                     part_id_size)
+    return files
