544e433d459b0cde4b5fef6f1df8b2e73e3755b8
Simon Dollé <simon@botify.com>
2014-08-25 15:28:43 +0200

    Add unit tests for generate_duplicate_stream()

--- tests/analysis/urls/transducers/test_metadata_duplicate.py
+++ tests/analysis/urls/transducers/test_metadata_duplicate.py
@@ -1,9 +1,10 @@
 import unittest
-
+from cdf.utils.hashing import string_to_int32
 from cdf.analysis.urls.transducers.metadata_duplicate import (
     count_metadata,
     notset_hash_value,
-    keep_only_first_metadata
+    keep_only_first_metadata,
+    generate_duplicate_stream
 )
 
 
@@ -54,4 +55,57 @@ class TestKeepOnlyFirstMetadata(unittest.TestCase):
             (2, 1, fake_hash, "foo title 2")
         ]
         self.assertEqual(expected_stream, list(actual_stream))
+
+
+class TestGenerateDuplicateStream(unittest.TestCase):
+    def test_nominal_case(self):
+        contents = [
+            (1, 1, string_to_int32("title1")),
+            (1, 4, string_to_int32("description1")),
+            (2, 1, string_to_int32("title2")),
+            (6, 1, string_to_int32("title1")),
+            (6, 4, string_to_int32("description2")),
+            (8, 4, string_to_int32("description1")),
+            (9, 4, string_to_int32("description1"))
+        ]
+        actual_stream = generate_duplicate_stream(contents, key=lambda x: (x[2], x[1]))
+
+        expected_stream = [
+            (1, 1, 2, True, [6]),
+            (1, 4, 3, True, [8, 9]),
+            (2, 1, 0, True, []),
+            (6, 1, 2, False, [1]),
+            (6, 4, 0, True, []),
+            (8, 4, 3, False, [1, 9]),
+            (9, 4, 3, False, [1, 8])
+        ]
+        #we do not test item order since order is not garanteed by generate_duplicate_stream()
+        self.assertItemsEqual(expected_stream, list(actual_stream))
+
+    def test_additional_criterion(self):
+        #The last element contains an additional criterion
+        contents = [
+            (1, 1, string_to_int32("title1"), True),
+            (1, 4, string_to_int32("description1"), False),
+            (2, 1, string_to_int32("title2"), True),
+            (6, 1, string_to_int32("title1"), True),
+            (6, 4, string_to_int32("description2"), True),
+            (8, 4, string_to_int32("description1"), True),
+            (9, 4, string_to_int32("description1"), True)
+        ]
+        #use the additional criterion in the key function
+        actual_stream = generate_duplicate_stream(contents, key=lambda x: (x[2], x[3], x[1]))
+
+        expected_stream = [
+            (1, 1, 2, True, [6]),
+            (1, 4, 0, True, []),
+            (2, 1, 0, True, []),
+            (6, 1, 2, False, [1]),
+            (6, 4, 0, True, []),
+            (8, 4, 2, True, [9]),
+            (9, 4, 2, False, [8])
+        ]
+        #we do not test item order since order is not garanteed by generate_duplicate_stream()
+        self.assertItemsEqual(expected_stream, list(actual_stream))
+
 
