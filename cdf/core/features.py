6594a143f75828209947616aa870f239c168ef70
Han JU <ju.han.felix@gmail.com>
2014-09-02 14:47:23 +0200

    copy get_urls_data_definition to core.features module, rename to assemble_data_format

--- cdf/core/features.py
+++ cdf/core/features.py
@@ -98,3 +98,16 @@ class Feature(object):
             logger.warning("Could not find an element named insights in '%s'.",
                            insights_module_name)
             return []
+
+
+def assemble_data_format():
+    """Assemble partial data format from each feature
+
+    :return: complete data format
+    """
+    urls_def = {}
+    for f in Feature.get_features():
+        for stream_def in f.get_streams_def():
+            if hasattr(stream_def, 'URL_DOCUMENT_MAPPING'):
+                urls_def.update(stream_def.URL_DOCUMENT_MAPPING)
+    return urls_def
