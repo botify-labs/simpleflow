9ac15054afe43ae702a47d073865a56a95b825d8
Simon Dollé <simon@botify.com>
2014-09-04 15:53:44 +0200

    Add unittest for make_zone_aware_metadata_duplicates_file()

--- tests/features/semantic_metadata/test_tasks.py
+++ tests/features/semantic_metadata/test_tasks.py
@@ -8,14 +8,17 @@ import shutil
 
 from cdf.utils.hashing import string_to_int32
 from cdf.analysis.urls.transducers.metadata_duplicate import notset_hash_value
+from cdf.features.main.streams import ZoneStreamDef, StrategicUrlStreamDef
 from cdf.features.semantic_metadata.streams import (
     ContentsCountStreamDef,
     ContentsDuplicateStreamDef,
+    ContentsZoneAwareDuplicateStreamDef,
     ContentsStreamDef
 )
 from cdf.features.semantic_metadata.tasks import (
     compute_metadata_count,
-    make_metadata_duplicates_file
+    make_metadata_duplicates_file,
+    make_zone_aware_metadata_duplicates_file
 )
 
 
@@ -143,3 +146,88 @@ class TestComputeMetadataDuplicateFile(unittest.TestCase):
             [9, 4, 3, False, [1, 8]]
         ]
         self.assertEqual(expected_stream, list(duplicate_stream))
+
+
+class TestMakeZoneAwareMetadataDuplicatesFile(unittest.TestCase):
+    def setUp(self):
+        self.bucket_name = "app.foo.com"
+        self.s3_uri = "s3://{}/crawl_result".format(self.bucket_name)
+        self.crawl_id = 10
+        self.tmp_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.tmp_dir)
+
+    @mock_s3
+    def test_nominal_case(self):
+        conn = boto.connect_s3()
+        bucket = conn.create_bucket(self.bucket_name)
+
+        first_part_size = 2
+        part_size = 10
+
+        contents = iter((
+            [1, 2, string_to_int32('My first H1'), 'My first H1'],
+            [1, 3, string_to_int32('My H2'), 'My H2'],
+            [1, 1, string_to_int32('My title'), 'My title'],
+            [1, 4, string_to_int32('My Desc'), 'My Desc'],
+            [2, 2, string_to_int32('My first H1'), 'My first H1'],
+            [2, 4, string_to_int32('My Desc'), 'My Desc'],
+            [3, 1, string_to_int32('My title'), 'My title'],
+            [3, 2, string_to_int32('My second H1'), 'My second H1'],
+            [3, 4, string_to_int32('My Desc'), 'My Desc'],
+        ))
+        ContentsStreamDef.persist(
+            iter(contents),
+            self.s3_uri,
+            first_part_size=first_part_size,
+            part_size=part_size
+        )
+
+        zones = iter([
+            (1, "en-US,https"),
+            (2, "en-US,https"),
+            (3, "en-US,https")
+        ])
+        ZoneStreamDef.persist(
+            iter(zones),
+            self.s3_uri,
+            first_part_size=first_part_size,
+            part_size=part_size
+        )
+
+        strategic_urls = iter([
+            (1, True, 0),
+            (2, False, 0),
+            (3, True, 0)
+        ])
+        StrategicUrlStreamDef.persist(
+            iter(strategic_urls),
+            self.s3_uri,
+            first_part_size=first_part_size,
+            part_size=part_size
+        )
+        output_files = make_zone_aware_metadata_duplicates_file(self.s3_uri,
+                                                                first_part_size,
+                                                                part_size)
+
+        expected_output_files = [
+            os.path.join(self.s3_uri, "urlcontentsduplicate_zoneaware.txt.0.gz"),
+            os.path.join(self.s3_uri, "urlcontentsduplicate_zoneaware.txt.1.gz"),
+        ]
+
+        self.assertEqual(expected_output_files, output_files)
+
+        actual_stream = ContentsZoneAwareDuplicateStreamDef.load(
+            self.s3_uri,
+            tmp_dir=self.tmp_dir
+        )
+        expected_stream = [
+            [1, 1, 2, True, [3]],
+            [1, 2, 0, True, []],
+            [1, 4, 2, True, [3]],
+            [3, 1, 2, False, [1]],
+            [3, 2, 0, True, []],
+            [3, 4, 2, False, [1]]
+        ]
+        self.assertEqual(expected_stream, list(actual_stream))
