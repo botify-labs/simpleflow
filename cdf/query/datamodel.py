a9166f7fcdedf4f67a70a6c8bd6985478f13484d
Simon Dollé <simon@botify.com>
2014-09-08 09:59:44 +0200

    Improve documentation.

--- cdf/query/datamodel.py
+++ cdf/query/datamodel.py
@@ -111,7 +111,12 @@ def get_groups(features_options):
 
 
 def make_fields_private(mapping):
-    """Make all the field of the mapping private
+    """Make all the field of the mapping private.
+    The initial intent of this function was to temporarly hide fields
+    that we do not want to display to the user because they are still under
+    development.
+    If you definitely want to make a field private, it is recommanded to
+    explicit add FIELD_RIGHTS.PRIVATE to its settings.
     :param mapping: input mapping as a dict field_name -> parameter dict
     :type mapping: dict
     :returns: dict - the modified mapping
