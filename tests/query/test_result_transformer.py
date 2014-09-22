e5eb02640398a3a9a2805fe18d0a41ef256e3fce
Han JU <ju.han.felix@gmail.com>
2014-09-22 11:37:30 +0200

    extract es mget mocking logic to cdf.testing.es_mock

--- tests/query/test_result_transformer.py
+++ tests/query/test_result_transformer.py
@@ -1,6 +1,9 @@
 import unittest
 import copy
 from mock import MagicMock
+from cdf.testing.es_mock import (
+    mock_es_mget
+)
 from cdf.metadata.url.backend import (
     ELASTICSEARCH_BACKEND,
     ElasticSearchBackend
@@ -19,50 +22,11 @@ CRAWL_NAME = 'crawl_%d' % CRAWL_ID
 REVISION_ID = 1
 ES_BACKEND = ELASTICSEARCH_BACKEND
 
-_MOCK_URL_MAPPING = {
-    '1:1': 'url1',
-    '1:2': 'url2',
-    '1:3': 'url3',
-    '1:4': 'url4',
-    '1:5': 'url5'
-}
-
-
-def _mock_es_mget(**kwargs):
-    assert 'body' in kwargs
-    body = kwargs['body']
-
-    docs = []
-    for _id in body['ids']:
-        url = _MOCK_URL_MAPPING.get(_id, '')
-        fields = {
-            u'url': url,
-            u'http_code': 0
-        }
-
-        crt_doc = {
-            u'_type': CRAWL_NAME,
-            u'found': True,
-            u'_index': ELASTICSEARCH_INDEX,
-            u'_source': fields,
-            u'_version': 1,
-            u'_id': _id
-        }
-        docs.append(crt_doc)
-
-    # url 6 not indexed for some reason
-    docs.append({u'found': False})
-
-    result = {
-        u'docs': docs
-    }
-    return result
-
 
 class TestIdToUrlTransformer(unittest.TestCase):
     def setUp(self):
         mock_conn = MagicMock()
-        mock_conn.mget = _mock_es_mget
+        mock_conn.mget = mock_es_mget
         self.es_conn = mock_conn
 
     def tearDown(self):
