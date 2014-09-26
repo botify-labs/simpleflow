f43e45e0c0e9884f979986a7a44d3bb1fd39c55b
Han JU <ju.han.felix@gmail.com>
2014-09-26 12:05:31 +0200

    remove unused constants

--- cdf/query/constants.py
+++ cdf/query/constants.py
@@ -23,34 +23,3 @@ SUB_AGG = 'subagg'
 # + the current aggregation (zero-filled on 2 numbers) to ensure
 # the sorting and correctly return results as a list
 METRIC_AGG_PREFIX = "metricagg"
-
-
-# Fields flags to return a specific field type rendering
-class RENDERING(Enum):
-    URL = 'url'
-    IMAGE_URL = 'image_url'
-    # Returns a 2-tuple list of (url, http_code)
-    URL_HTTP_CODE = 'url_http_code'
-    # Return a dict {"url": {"url": url, "crawled": bool_crawled}, "status": ["follow"]}
-    URL_LINK_STATUS = 'url_link_status'
-    # Return a dict {"url": url, "crawled": bool_crawled}
-    URL_STATUS = 'url_status'
-    # Returns a map dict:
-    # {'text': ["My text", "My other text", ..], 'nb': [20, 10..]}
-    STRING_NB_MAP = 'string_nb_map'
-    TIME_SEC = 'time_sec'
-    TIME_MILLISEC = 'time_millisec'
-    TIME_MIN = 'time_min'
-    PERCENT = 'percent'
-
-
-class FIELD_RIGHTS(Enum):
-    # This field is private and cannot be requested outside
-    PRIVATE = 'private'
-    # This field can be called in filtering operations
-    FILTERS = 'filters'
-    # This field can be called in filtering operations
-    # but just to check if it exists
-    FILTERS_EXIST = 'filters_exist'
-    # This field can only be selected for results
-    SELECT = 'select'
