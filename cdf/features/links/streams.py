49808b1177626bd4512659f62ec4630ab9d92665
Simon Dollé <simon@botify.com>
2014-10-07 17:34:10 +0200

    Implement InlinksPercentilesStreamDef.process_document()

--- cdf/features/links/streams.py
+++ cdf/features/links/streams.py
@@ -1079,3 +1079,23 @@ class InlinksPercentilesStreamDef(StreamDefBase):
         ('percentile_id', int),
         ('nb_follow_inlinks', int)
     )
+
+    URL_DOCUMENT_DEFAULT_GROUP = GROUPS.inlinks.name
+    URL_DOCUMENT_MAPPING = {
+        # erroneous outgoing internal links
+        "inlinks_internal.percentile_id": {
+            "type": INT_TYPE,
+            "verbose_name": "Percentile id.",
+            "order": 101,
+            "settings": {
+                ES_DOC_VALUE,
+                AGG_NUMERICAL,
+                AGG_CATEGORICAL,
+                FIELD_RIGHTS.ADMIN
+            }
+        },
+    }
+
+    def process_document(self, document, input_stream):
+        _, percentile_id, _ = input_stream
+        document["inlinks_internal"]["percentile_id"] = percentile_id
