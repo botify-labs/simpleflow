162d87a6156c07c943c9d23c7c97ae3a2ae3279d
Simon Dollé <simon@botify.com>
2014-10-20 12:28:03 +0200

    Implement enumerate_partitions()
    This method is required to handle cases where partition is are
    discontinuous.

--- cdf/utils/remote_files.py
+++ cdf/utils/remote_files.py
@@ -1,3 +1,4 @@
+import re
 from cdf.utils import s3, path
 
 
@@ -11,3 +12,28 @@ def nb_parts_from_crawl_location(uri):
     else:
         files = path.list_files(uri, regexp=regexp)
     return len(files)
+
+
+def enumerate_partitions(uri):
+    """
+    Return the list of partition ids
+    :param uri: the crawl data location (s3_uri)
+    :type uri: str
+    :returns: list - sorted list of urlids
+    """
+    regexp = r'urlids\.txt\.([0-9]+)\.gz'
+    if s3.is_s3_uri(uri):
+        files = s3.list_files(uri, regexp=regexp)
+        files = [f.name for f in files]
+    else:
+        files = path.list_files(uri, regexp=regexp)
+    pattern = r"urlids\.txt\.(\d+)\.gz$"
+    pattern = re.compile(pattern)
+    result = []
+    for f in files:
+        m = pattern.search(f)
+        if m is None:
+            continue
+        result.append(int(m.group(1)))
+    result = sorted(result)
+    return result
---
+++ tests/utils/test_remote_files.py
@@ -0,0 +1,44 @@
+import unittest
+import boto
+from moto import mock_s3
+from cdf.utils.remote_files import enumerate_partitions
+
+
+class TestEnumeratePartitions(unittest.TestCase):
+
+    def _get_bucket(self):
+        bucket = 'test_bucket'
+        s3 = boto.connect_s3()
+        test_bucket = s3.create_bucket(bucket)
+        return test_bucket
+
+    def _create_file(self, bucket, file_name):
+        key = boto.s3.key.Key(bucket)
+        key.name = file_name
+        key.set_contents_from_string("")
+
+    @mock_s3
+    def test_nominal_case(self):
+        test_bucket = self._get_bucket()
+        self._create_file(test_bucket, "urlids.txt.0.gz")
+        self._create_file(test_bucket, "urlids.txt.4.gz")
+
+        self.assertEquals([0, 4], enumerate_partitions("s3://test_bucket/"))
+
+    @mock_s3
+    def test_misc_files(self):
+        test_bucket = self._get_bucket()
+        self._create_file(test_bucket, "urlids.txt.0.gz")
+        self._create_file(test_bucket, "urlinfos.txt.4.gz")
+
+        self.assertEquals([0], enumerate_partitions("s3://test_bucket/"))
+
+    @mock_s3
+    def test_regex_special_character(self):
+        test_bucket = self._get_bucket()
+        #check that "." in regex is not interpret as wildcard
+        self._create_file(test_bucket, "urlids_txt_0_gz")
+        #check that only files ending with the patterns are considered
+        self._create_file(test_bucket, "urlids_txt_0_gz.foo")
+
+        self.assertEquals([], enumerate_partitions("s3://test_bucket/"))
