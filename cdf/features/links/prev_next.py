5e98fc7f67102b6e935022ec876f76b701b8ab59
Simon Dollé <simon@botify.com>
2014-11-18 17:43:48 +0100

    Improve documentation

--- cdf/features/links/prev_next.py
+++ cdf/features/links/prev_next.py
@@ -4,7 +4,8 @@ from cdf.features.links.streams import InlinksStreamDef
 
 def compute_prev_next_stream(inlinks_stream):
     """Compute a prev/next stream (based on PrevNextStreamDef) from a
-    inlinks stream (based on InlinksStreamDef)
+    inlinks stream (based on InlinksStreamDef).
+    Only follow links are considered.
     :param inlinks_stream: the input stream (based on InlinksStreamDef)
     :type inlinks_stream: iterable
     :returns: iterable - based on PrevNextStreamDef
@@ -12,6 +13,7 @@ def compute_prev_next_stream(inlinks_stream):
     id_index = InlinksStreamDef.field_idx('id')
     follow_mask_index = InlinksStreamDef.field_idx('follow')
 
+    #consider only follow links
     inlinks_stream = itertools.ifilter(lambda x: 'follow' in x[follow_mask_index], inlinks_stream)
     for url_id, inlinks in itertools.groupby(inlinks_stream, key=lambda x: x[id_index]):
         receives_prev = False
