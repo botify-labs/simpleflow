578feaf9ccce4c59e134c56344410fd2745a8c55
Simon Dollé <simon@botify.com>
2014-11-21 11:05:47 +0100

    Use fields_idx() in get_url_to_id_dict_from_stream()
    This way, if IdStreamDef changes the function will still work.

--- cdf/features/main/utils.py
+++ cdf/features/main/utils.py
@@ -1,9 +1,16 @@
+from operator import itemgetter
+from cdf.features.main.streams import IdStreamDef
+
 def get_url_to_id_dict_from_stream(stream):
     """
     Return a dictionnary of urls keys mapping to local url_ids
     """
+    field_indexes = IdStreamDef.fields_idx(
+        ["id", "protocol", "host", "path", "query_string"]
+    )
     d = {}
-    for url_id, protocol, host, path, query_string in stream:
+    for elt in stream:
+        url_id, protocol, host, path, query_string = itemgetter(*field_indexes)(elt)
         d[protocol + '://' + ''.join((host, path, query_string))] = url_id
     return d
 
