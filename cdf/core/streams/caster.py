a6ea2dbb6896e1ce2c904109de04e3bdbc45a573
Simon Dollé <simon@botify.com>
2014-12-01 17:49:13 +0100

    Create class FieldCaster
    It should speed up the process even if some of the fields are
    potentially missing.

--- cdf/core/streams/caster.py
+++ cdf/core/streams/caster.py
@@ -51,6 +51,38 @@ def return_value(value, cast_func, options):
     return cast_func(value)
 
 
+class FieldCaster(object):
+    def cast(value):
+        pass
+
+
+class BasicFieldCaster(FieldCaster):
+    def __init__(self, cast):
+        self._cast = cast
+
+    def cast(self, value):
+        return self._cast(value)
+
+
+class AdvancedFieldCaster(FieldCaster):
+    def __init__(self, cast, options):
+        self._cast = cast
+        self.options = options
+        self.missing_value = None
+        if MISSING_OPTION in self.options:
+            self.missing_value = self._cast(self.options[MISSING_OPTION])
+        elif DEFAULT_OPTION in self.options:
+            self.missing_value = self._cast(self.options[DEFAULT_OPTION])
+        else:
+            self.missing_value = self._cast('')
+
+    def cast(self, value):
+        if value == MISSING_VALUE:
+            return self.missing_value
+        elif value == '' and DEFAULT_OPTION in self.options:
+            return self._cast(self.options[DEFAULT_OPTION])
+        return self._cast(value)
+
 
 class Caster(object):
     """
@@ -65,19 +97,26 @@ class Caster(object):
             self._fields = fields
             return
 
+        self.casters = []
         for field in fields:
             # Add empty options if the field definition has only 2 values (field_name, caster_func)
             if len(field) == 2:
                 self._fields.append(field + ({},))
+                name, cast = field
+                caster = BasicFieldCaster(cast)
+                self.casters.append(caster)
             else:
                 self._fields.append(field)
+                name, cast, options = field
+                caster = AdvancedFieldCaster(cast, options)
+                self.casters.append(caster)
 
     def cast_line(self, line):
-        return [(return_value(value, cast, options) if cast else value) for
-                (name, cast, options), value in izip_longest(self._fields, line, fillvalue=MISSING_VALUE)]
+        return [caster.cast(value) for
+                caster, value in izip_longest(self.casters, line, fillvalue=MISSING_VALUE)]
 
     def cast(self, iterable):
-        if self.no_missing_field:
+        if self.no_missing_field is True:
             #simple case, we can simply apply the cast functions
             for i in iterable:
                 yield [cast(value) for (name, cast), value in zip(self._fields, i)]
