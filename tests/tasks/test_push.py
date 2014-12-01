d179b5b8e2cb2ac920b677dd702e885cb87f4448
Han JU <ju.han.felix@gmail.com>
2014-12-01 11:01:49 +0100

    improve test cases

--- tests/tasks/test_push.py
+++ tests/tasks/test_push.py
@@ -9,22 +9,33 @@ def _get_mock_bulk(oks, errors):
         return oks, errors
     return _mock
 
+# max 5% error rate in this test
+MAX_ERROR_RATE = 0.05
+
 
 class TestDocumentPushError(unittest.TestCase):
     def setUp(self):
         self.stream = iter(['doc1', 'doc2', 'doc3'])
 
+    @mock.patch('cdf.tasks.url_data.bulk', _get_mock_bulk(1000, 0))
+    def test_push_all_ok(self):
+        # should terminate normally
+        push_document_stream(self.stream, None, "", "",
+                             max_error_rate=MAX_ERROR_RATE)
+
     @mock.patch('cdf.tasks.url_data.bulk', _get_mock_bulk(1000, 1))
     def test_push_accept(self):
         # should terminate normally
-        push_document_stream(self.stream, None, "", "")
+        push_document_stream(self.stream, None, "", "",
+                             max_error_rate=MAX_ERROR_RATE)
 
-    @mock.patch('cdf.tasks.url_data.bulk', _get_mock_bulk(50, 50))
+    @mock.patch('cdf.tasks.url_data.bulk', _get_mock_bulk(94, 6))
     def test_push_not_accept(self):
         # should raise exception
         self.assertRaises(
             Exception,
             push_document_stream,
             self.stream,  # mock stream
-            None, "", ""  # not important param
+            None, "", "",  # not important param
+            MAX_ERROR_RATE
         )
