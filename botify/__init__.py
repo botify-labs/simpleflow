fa9af1930661a10a024238976e065a89095e025b
Greg Leclercq <greg@sem.io>
2015-01-09 11:30:36 +0100

    Add comment about share namespaces

--- botify/__init__.py
+++ botify/__init__.py
@@ -1,3 +1,6 @@
+# The lines below are required to share the ``botify`` namespace with other
+# packages.  For example, being able to install on the same environment
+# ``botify.common`` and ``botify.other_package``.
 try:
     __import__('pkg_resources').declare_namespace(__name__)
 except ImportError:
