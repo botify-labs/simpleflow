22709812d5c4bca8cb6483dfe096e11b8727da46
Han JU <ju.han.felix@gmail.com>
2015-01-15 16:40:01 +0100

    hide pyhash import in function

--- cdf/utils/hashing.py
+++ cdf/utils/hashing.py
@@ -1,13 +1,13 @@
-import pyhash
-hasher_64 = pyhash.fnv1_64()
-hasher_32 = pyhash.fnv1_32()
-
 from cdf.utils.convert import to_int64, to_int32
 
 
 def string_to_int64(string):
+    import pyhash
+    hasher_64 = pyhash.fnv1_64()
     return to_int64(hasher_64(string))
 
 
 def string_to_int32(string):
+    import pyhash
+    hasher_32 = pyhash.fnv1_32()
     return to_int32(hasher_32(string))
