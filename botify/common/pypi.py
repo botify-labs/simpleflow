53967e83d1e88da83c02a73dbe838669a4f16486
Greg Leclercq <greg@botify.com>
2015-03-30 15:49:39 +0200

    Update pypi: add parameter dist_type

--- botify/common/pypi.py
+++ botify/common/pypi.py
@@ -1,11 +1,19 @@
 import subprocess
 
+SDIST = 'sdist'
+BDIST_WHEEL = 'bdist_wheel'
+
+DIST_TYPES = (
+    SDIST,
+    BDIST_WHEEL,
+)
+
 
 class Pypi(object):
     def __init__(self, name):
         self.name = name
 
-    def upload(self, dry_run=False):
+    def upload(self, dist_type=BDIST_WHEEL, dry_run=False):
         """Create the python package and upload it to pypi.
 
         :param dry_run: if True, nothing is actually done.
@@ -14,8 +22,15 @@ class Pypi(object):
         :type dry_run: bool.
 
         """
-        command = 'python setup.py bdist_wheel upload -r {repo}'.format(
-            repo=self.name)
+        if dist_type not in DIST_TYPES:
+            raise ValueError('invalid dist type "{}": must be in {}'.format(
+                dist_type,
+                ', '.join(DIST_TYPES)))
+
+        command = 'python setup.py {dist_type} upload -r {repo}'.format(
+            dist_type=dist_type,
+            repo=self.name,
+        )
 
         if not dry_run:
             print subprocess.check_output(command, shell=True)
