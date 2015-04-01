f8b2a0d7a679e3ad9244c48782c84846c848c57c
Greg Leclercq <greg@botify.com>
2015-04-01 14:15:04 +0000

    Update utils.kvstore: make it a package
    Provide two implementation modules for plyvel and pyleveldb (in
    kvstore.leveldb). The __init__.py file dispatches to the first
    available implementation.

---
+++ cdf/utils/kvstore/leveldb.py
@@ -0,0 +1,96 @@
+from __future__ import absolute_import
+
+import logging
+import shutil
+
+import leveldb
+
+from . import base
+from . import exceptions
+from . import constants
+
+
+logger = logging.getLogger(__name__)
+
+
+class LevelDB(base.KVStore):
+    def __init__(self, path):
+        self.path = path
+        self.db = None
+
+    def _check(self):
+        """Check that the DB wrapper object is operational
+        """
+        if self.db is None:
+            raise exceptions.KVStoreException('DB not initiated ...')
+
+    def open(self, **configs):
+        """Open the DB
+        """
+        self.db = leveldb.LevelDB(self.path, create_if_missing=True, **configs)
+
+    def close(self):
+        """Close the DB
+        """
+        self._check()
+        del self.db
+        self.db = None
+
+    def destroy(self):
+        self.close()
+        # Please ensure there's a sane value in ``self.path`` as
+        # :func:`shutil.rmtree` is recursive.
+        shutil.rmtree(self.path)
+
+    def reopen(self, **configs):
+        """Close and then open the DB, potentially with other configurations
+        """
+        self._check()
+        self.close()
+        self.open(**configs)
+
+    def batch_write(self, kv_stream, batch_size=constants.DEFAULT_BATCH_SIZE):
+        """Batch write a key-value stream into the DB
+
+        Note it's generally better to configure the DB with large write buffer
+        `reopen` can be used upon batch write to change the configuration
+
+        :param kv_stream: a key-value pair stream
+        :param batch_size: size of each write batch
+        """
+        self._check()
+        batch = leveldb.WriteBatch()
+
+        for count, (k, v) in enumerate(kv_stream):
+            batch.Put(str(k), str(v))
+            if count != 0 and count % batch_size == 0:
+                self.db.Write(batch)
+                batch = leveldb.WriteBatch()
+                logger.debug(
+                    "Put {} records in DB {}".format(count, self.path))
+
+        self.db.Write(batch)
+
+    def iterator(self):
+        """Returns an iterator for key-ordered iteration
+        """
+        self._check()
+        # iteration means we do a full pass on the data
+        # but no random lookup, cache is not relevant in
+        # this case
+        return self.db.RangeIter(fill_cache=False)
+
+    def put(self, key, value):
+        """Put a key-value pair
+        """
+        self._check()
+        self.db.Put(key, value)
+
+    def get(self, key):
+        """Get a value from a key
+        """
+        self._check()
+        return self.db.Get(key)
+
+    def __del__(self):
+        self.destroy()
---
+++ tests/utils/test_kvstore.py
@@ -0,0 +1,55 @@
+from __future__ import absolute_import
+
+import unittest
+import tempfile
+import os.path
+import logging
+import gc
+
+from cdf.utils.kvstore import leveldb
+from cdf.utils.kvstore import exceptions
+
+
+logger = logging.getLogger(__name__)
+
+
+class Base(object):
+    def setUp(self):
+        self.path = tempfile.mkdtemp()
+        self.db = self.cls(self.path)
+        self.db.open()
+
+    def test_open_del(self):
+        del self.db
+        self.db = None
+        gc.collect()
+
+        self.assertFalse(os.path.exists(self.path))
+
+    def test_put_get(self):
+        db = self.db
+        db.put('a', '1')
+        db.put('b', '2')
+        self.assertEquals(db.get('a'), '1')
+        self.assertEquals(db.get('b'), '2')
+
+    def test_cannot_destroy_wrong_path(self):
+        with tempfile.NamedTemporaryFile() as tmp:
+            db = self.cls(tmp.name)
+            self.assertRaises(exceptions.KVStoreException)
+            del db
+            gc.collect()
+            self.assertTrue(os.path.exists(tmp.name))
+
+
+try:
+    from cdf.utils.kvstore import plyvel
+
+    class TestPlyvel(Base, unittest.TestCase):
+        cls = plyvel.LevelDB
+except ImportError as err:
+    logger.warning('cannot load test for plyvel: {}'.format(err))
+
+
+class TestLeveldb(Base, unittest.TestCase):
+    cls = leveldb.LevelDB
