7df47a19430c7a6d8a67c3201c39ecefbdd4ab75
zeb <yves@botify.com>
2015-04-16 15:39:40 +0200

    regex_checking: wip

--- cdf/utils/regex_checking.py
+++ cdf/utils/regex_checking.py
@@ -1,12 +1,166 @@
 __author__ = 'zeb'
 
+from enum import Enum
+# import re
+
+# Note: doesn't contain '.'
+_specials = set(r"^.*+?\|[(){$")
+
+
+class RegexError(Exception):
+    """
+    Exception on parsing a RE.
+    """
+    def __init__(self, token, c, pos):
+        super(RegexError, self).__init__()
+        self.token = token
+        self.c = c
+        self.pos = pos
+
+
+class Token(Enum):
+    Normal = 1,
+    KnownEscape = 2,
+    UnrecognizedEscapeError = 3,
+    BackslashAtEOLError = 4,
+    Group = 5,
+    BadGroupError = 6,
+    EndGroup = 7,  # Internal
+    Kleene = 8,
+    Or = 9,
+    Anchor = 10,
+    Any = 11,
+    QMark = 12,
+    QMarkThatsEnough = 13,  # Internal. To address 'x*??', 'x???' ...
+    Class = 14,
+    BadClassError = 15,
+
+
+TOKEN_GROUP_ = {Token.Normal, Token.Any, Token.KnownEscape, Token.Group, Token.Class}
+
+
 class ParserState(object):
+    """
+    State of the RE parser. Iterable, returning (Token, str).
+    """
+
+    # XXX python doesn't support z, only Z; re2 supports z and not Z; .Net accepts both.
+    KNOWN_ESCAPES = set("wsdWSD" + "Az" + "trn")
+
     def __init__(self, regex):
-        self.regex = regex
-        self.pos = -1
+        """
+        Initialize the ParserState
+        :param regex: The regex
+        :type regex: str
+        """
+        self.pos = 0
+        self.len = len(regex)
+        self.regex = regex + ' '
+        self.nparens = 0
 
+    def __iter__(self):
+        return self
 
-_specials = set(r"*+?\[](){}")
+    def next(self):
+        """
+        Iterator.
+        :return:Token, str
+        :rtype: tuple
+        """
+        i = self.pos
+        if i < self.len:
+            self.pos += 1
+            return self._tokenize(self.regex[i])
+        raise StopIteration()
+
+    def _tokenize(self, c):
+        if c not in _specials:
+            return Token.Normal, c
+        if c == '.':
+            return Token.Any, c
+        if c == '\\':
+            return self._get_escaped()
+        if c == '(':
+            return self._get_parens()
+        if c == ')':
+            self.nparens -= 1
+            if self.nparens < 0:
+                return Token.BadGroupError, None
+            return Token.EndGroup, None
+        if c in '*+':
+            return Token.Kleene, c
+        if c == '?':
+            return Token.QMark, c
+        if c in '^$':
+            return Token.Anchor, c
+        if c == '|':
+            return Token.Or, c
+        if c == '[':
+            return self._get_char_class()
+
+    def _get_escaped(self):
+        i = self.pos
+        if i < self.len:
+            self.pos += 1
+            return self._classify_escape(self.regex[i])
+        return Token.BackslashAtEOLError, '\\'
+        # raise RegexError("\\ at end of expression")
+
+    def _classify_escape(self, c):
+        """
+        Classify the escape.
+        :param c:
+        :type c: str
+        :return: Token, c
+        :rtype: tuple
+        """
+        if c in _specials or c in ']}':
+            return Token.Normal, c
+        if c in self.KNOWN_ESCAPES:
+            return Token.KnownEscape, c
+        return Token.UnrecognizedEscapeError, c
+        # raise RegexError("Unsupported escape sequence")
+
+    def _get_parens(self):
+        content = []
+        i = self.pos
+        if i >= self.len:
+            return Token.BadGroupError, None
+        self.nparens += 1
+        for token, c in self:
+            if token == Token.BadGroupError:
+                return token, c
+            if token == Token.EndGroup:
+                return Token.Group, content
+            content.append((token, c))
+        return Token.BadGroupError, content
+
+    def _get_char_class(self):
+        content = []
+        reverse = False
+        i = self.pos
+        if i >= self.len:
+            return Token.BadClassError, None
+        c1 = self.regex[i]
+        if i + 1 >= self.len:
+            return Token.BadClassError, None
+        # FIXME [[:xxx:]]
+        if c1 == '^':
+            reverse = True
+            self.pos += 1
+            i += 1
+            if i >= self.len:
+                return Token.BadClassError, None
+            c1 = self.regex[i]
+        while 1:
+            content.append(c1)
+            i += 1
+            if i >= self.len:
+                return Token.BadClassError, None
+            c1 = self.regex[i]
+            if c1 == ']':
+                break
+
 
 def check(regex):
     # Quick checks
@@ -21,6 +175,25 @@ def check(regex):
         return True
 
     parser = ParserState(regex)
+    prev_token = None
+    for tc in parser:
+        if not tc:
+            raise RegexError(None, None, parser.pos)
+        token, c = tc
+        ok = False
+        if token in TOKEN_GROUP_ or token in (Token.Anchor, Token.Or):
+            ok = True
+        elif token == Token.Kleene:
+            ok = prev_token in TOKEN_GROUP_ or prev_token == Token.Anchor
+        elif token == Token.QMark:
+            ok = prev_token in TOKEN_GROUP_
+            if not ok:
+                ok = prev_token in (Token.Anchor, Token.Kleene, Token.QMark)
+                if ok:
+                    token = Token.QMarkThatsEnough
+        if ok:
+            prev_token = token
+            continue
+        raise RegexError(token, c, parser.pos)
 
-    # TODO we refuse everything here
-    return False
+    return True
--- tests/utils/test_regex_checking.py
+++ tests/utils/test_regex_checking.py
@@ -2,8 +2,9 @@ __author__ = 'zeb'
 
 import unittest
 
-import re
-from cdf.utils.regex_checking import check
+# import re
+from cdf.utils.regex_checking import *  # check, ParserState, Token
+
 
 class BasicTests(unittest.TestCase):
     def test_empty(self):
@@ -13,3 +14,311 @@ class BasicTests(unittest.TestCase):
     def test_simple(self):
         s = "toto"
         self.assertTrue(check(s))
+
+    def test_parens(self):
+        s = r"(abc)"
+        self.assertTrue(check(s))
+
+    def test_kleene_start(self):
+        s = r"*"
+        with self.assertRaises(RegexError):
+            check(s)
+
+    def test_kleene_double(self):
+        s = r"*+"
+        with self.assertRaises(RegexError):
+            check(s)
+
+    def test_kleene_double_2(self):
+        s = r".**"
+        with self.assertRaises(RegexError):
+            check(s)
+
+    def test_kleene_or(self):
+        s = r"|+"
+        with self.assertRaises(RegexError):
+            check(s)
+
+    def test_kleene_ok1(self):
+        s = r"a+b*"
+        self.assertTrue(check(s))
+
+    def test_kleene_ok2(self):
+        s = r"\d+\D*"
+        self.assertTrue(check(s))
+
+    def test_kleene_ok3(self):
+        s = r"(xy)+(a|b)*"
+        self.assertTrue(check(s))
+
+    def test_kleene_ok4(self):
+        s = r".+.*"
+        self.assertTrue(check(s))
+
+    def test_or_1(self):
+        s = r"a|bc"
+        self.assertTrue(check(s))
+
+    def test_or_2(self):
+        s = r"|"
+        self.assertTrue(check(s))
+
+    def test_named(self):
+        s = r"(?P<toto>...)"
+        self.assertTrue(check(s))
+
+    def test_q_1(self):
+        s = r"?"
+        with self.assertRaises(RegexError):
+            check(s)
+
+    def test_q_2(self):
+        s = r"??"
+        with self.assertRaises(RegexError):
+            check(s)
+
+    def test_q_3(self):
+        s = r".?ab?"
+        self.assertTrue(check(s))
+
+    def test_q_4(self):
+        s = r".*?ab+?"
+        self.assertTrue(check(s))
+
+    def test_q_5(self):
+        s = r".??"
+        self.assertTrue(check(s))
+
+    def test_q_6(self):
+        s = r".???"
+        with self.assertRaises(RegexError):
+            check(s)
+
+    def test_q_7(self):
+        s = r".+??"
+        with self.assertRaises(RegexError):
+            check(s)
+
+    def test_brace_open(self):
+        s = r"["
+        with self.assertRaises(RegexError):
+            check(s)
+
+    def test_brace_open_not(self):
+        s = r"[^"
+        with self.assertRaises(RegexError):
+            check(s)
+
+    def test_class_empty(self):
+        s = r"[]"
+        with self.assertRaises(RegexError):
+            check(s)
+
+    def test_class_empty_not(self):
+        s = r"[^]"
+        with self.assertRaises(RegexError):
+            check(s)
+
+    def test_class_bracket_open(self):
+        s = r"[[]"
+        self.assertTrue(check(s))
+
+    def test_class_bracket_closed(self):
+        s = r"[]]"
+        self.assertTrue(check(s))
+
+    def test_class_bracket_open_ab(self):
+        s = r"[[ab]"
+        self.assertTrue(check(s))
+
+    def test_class_bracket_closed_ab(self):
+        s = r"[]ab]"
+        self.assertTrue(check(s))
+
+    def test_class_bracket_not_open(self):
+        s = r"[^[]"
+        self.assertTrue(check(s))
+
+    def test_class_bracket_not_closed(self):
+        s = r"[^]]"
+        self.assertTrue(check(s))
+
+    def test_class_bracket_not_open_ab(self):
+        s = r"[^[ab]"
+        self.assertTrue(check(s))
+
+    def test_class_bracket_not_closed_ab(self):
+        s = r"[^]ab]"
+        self.assertTrue(check(s))
+
+    def test_class_bracket_a(self):
+        s = r"[a]"
+        self.assertTrue(check(s))
+
+    def test_class_bracket_abc(self):
+        s = r"[abc]"
+        self.assertTrue(check(s))
+
+    def test_class_bracket_cba(self):
+        s = r"[cba]"
+        self.assertTrue(check(s))
+
+    def test_class_bracket_ac(self):
+        s = r"[a-c]"
+        self.assertTrue(check(s))
+
+    def test_class_bracket_ca(self):
+        s = r"[c-a]"
+        self.assertTrue(check(s))
+
+    def test_class_bracket_acAC(self):
+        s = r"[a-cA-C]"
+        with self.assertRaises(RegexError):
+            check(s)
+
+    def test_class_bracket_not_abc(self):
+        s = r"[^abc]"
+        self.assertTrue(check(s))
+
+
+class ParserStateTests(unittest.TestCase):
+    def test_empty(self):
+        ps = ParserState("")
+        with self.assertRaises(StopIteration):
+            ps.next()
+
+    def test_simple(self):
+        ps = ParserState("xy")
+        x = ps.next()
+        self.assertEqual((Token.Normal, 'x'), x)
+        x = ps.next()
+        self.assertEqual((Token.Normal, 'y'), x)
+        with self.assertRaises(StopIteration):
+            ps.next()
+
+    def test_escape_special(self):
+        s = r"*+?\[](){}.^$|"
+        s_escaped = '\\' + '\\'.join(s)
+        ps = ParserState(s_escaped)
+        # zz = list(zip(ps, s))
+        for rx, x in zip(ps, s):
+            self.assertEqual((Token.Normal, x), rx)
+
+    def test_escape_eol(self):
+        ps = ParserState("\\")
+        x = ps.next()
+        self.assertEqual((Token.BackslashAtEOLError, '\\'), x)
+        with self.assertRaises(StopIteration):
+            ps.next()
+
+    def test_known_escape(self):
+        s = "wdsWDS"
+        s_escaped = '\\' + '\\'.join(s)
+        ps = ParserState(s_escaped)
+        # zz = list(zip(ps, s))
+        for rx, x in zip(ps, s):
+            self.assertEqual((Token.KnownEscape, x), rx)
+
+    def test_unknown_escape(self):
+        s = "xGpP%0123456789"
+        s_escaped = '\\' + '\\'.join(s)
+        ps = ParserState(s_escaped)
+        # zz = list(zip(ps, s))
+        for rx, x in zip(ps, s):
+            self.assertEqual((Token.UnrecognizedEscapeError, x), rx)
+
+    def test_empty_parens(self):
+        ps = ParserState("()")
+        for rx in ps:
+            self.assertEqual((Token.Group, []), rx)
+
+    def test_left_parens(self):
+        ps = ParserState("(")
+        for rx in ps:
+            self.assertEqual((Token.BadGroupError, None), rx)
+
+    def test_right_parens(self):
+        ps = ParserState(")")
+        for rx in ps:
+            self.assertEqual((Token.BadGroupError, None), rx)
+
+    def test_left_parens_2(self):
+        ps = ParserState("(xxx")
+        for rx in ps:
+            self.assertEqual(Token.BadGroupError, rx[0])
+
+    def test_left_parens_3(self):
+        ps = ParserState("(xxx(x)x")
+        for rx in ps:
+            self.assertEqual(Token.BadGroupError, rx[0])
+
+    def test_left_parens_4(self):
+        ps = ParserState("(xx\\)")
+        for rx in ps:
+            self.assertEqual(Token.BadGroupError, rx[0])
+
+    def test_right_parens_2(self):
+        ps = ParserState("xxx)x")
+        for tok, c in ps:
+            if tok == Token.Normal:
+                self.assertEqual('x', c)
+            else:
+                self.assertEqual(Token.BadGroupError, tok)
+
+    def test_parens_x(self):
+        ps = ParserState("(x)")
+        for rx in ps:
+            self.assertEqual((Token.Group, [(Token.Normal, 'x')]), rx)
+
+    def test_parens_xyz(self):
+        ps = ParserState("((xy)(z))")
+        for rx in ps:
+            self.assertEqual((Token.Group, [
+                (Token.Group,
+                 [(Token.Normal, 'x'), (Token.Normal, 'y')]),
+                (Token.Group,
+                 [(Token.Normal, 'z')])
+            ]), rx)
+
+    def test_star(self):
+        ps = ParserState("*")
+        tok, c = ps.next()
+        self.assertEqual(Token.Kleene, tok)
+        self.assertEqual('*', c)
+
+    def test_plus(self):
+        ps = ParserState("+")
+        tok, c = ps.next()
+        self.assertEqual(Token.Kleene, tok)
+        self.assertEqual('+', c)
+
+    def test_bracket_open(self):
+        s = r"["
+        ps = ParserState(s)
+        tok, c = ps.next()
+        self.assertEqual(Token.BadClassError, tok)
+
+    def test_bracket_closed(self):
+        s = r"]"
+        ps = ParserState(s)
+        tok, c = ps.next()
+        self.assertEqual(Token.Normal, tok)
+        self.assertEqual(']', c)
+
+    def test_class_bracket_open(self):
+        s = r"[[]"
+        ps = ParserState(s)
+        tok, c = ps.next()
+        self.assertEqual(Token.Class, tok)
+        self.assertEqual([(Token.Normal, '[')], c)
+        with self.assertRaises(StopIteration):
+            ps.next()
+
+    def test_class_bracket_closed(self):
+        s = r"[]]"
+        ps = ParserState(s)
+        tok, c = ps.next()
+        self.assertEqual(Token.Class, tok)
+        self.assertEqual([(Token.Normal, ']')], c)
+        with self.assertRaises(StopIteration):
+            ps.next()
