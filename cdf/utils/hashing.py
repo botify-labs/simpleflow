92d49bff7e301a1d2310093f9cd616f3cb5973dd
Han JU <ju.han.felix@gmail.com>
2015-05-15 12:10:41 +0200

    replace util/hashing by botify-common's hash

--- cdf/utils/hashing.py
+++ cdf/utils/hashing.py
@@ -1,13 +1,5 @@
-from cdf.utils.convert import to_int64, to_int32
+from botify.common.hash import fnv
 
 
-def string_to_int64(string):
-    import pyhash
-    hasher_64 = pyhash.fnv1_64()
-    return to_int64(hasher_64(string))
-
-
-def string_to_int32(string):
-    import pyhash
-    hasher_32 = pyhash.fnv1_32()
-    return to_int32(hasher_32(string))
+string_to_int64 = fnv.string_to_int64
+string_to_int32 = fnv.string_to_int32
