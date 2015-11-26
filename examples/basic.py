4563ad431102bd578582dfd6af41fe68ac7c6c26
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2015-11-26 15:50:06 +0100

    Revert "Update example workflow to show you can use classes"
    This reverts commit dbce79102efa8fee233af95939f1ff0b9d060b00.

--- examples/basic.py
+++ examples/basic.py
@@ -16,13 +16,11 @@ def increment(x):
 def double(x):
     return x * 2
 
-# A simpleflow activity can be any callable, so a function works, but a class
-# will also work given the processing happens in __init__()
+
 @activity.with_attributes(task_list='quickstart', version='example')
-class Delay(object):
-    def __init__(self, t, x):
-        time.sleep(t)
-        return x
+def delay(t, x):
+    time.sleep(t)
+    return x
 
 
 class BasicWorkflow(Workflow):
@@ -32,7 +30,7 @@ class BasicWorkflow(Workflow):
 
     def run(self, x, t=30):
         y = self.submit(increment, x)
-        yy = self.submit(Delay, t, y)
+        yy = self.submit(delay, t, y)
         z = self.submit(double, y)
 
         print '({x} + 1) * 2 = {result}'.format(
