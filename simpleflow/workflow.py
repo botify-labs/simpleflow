3f2d02ec10e8f2edf4676cfde27250cb5b9765ac
ampelmann <thomas@botify.com>
2015-12-16 14:36:46 +0000

    rename func to activity in Workflow.submit

--- simpleflow/workflow.py
+++ simpleflow/workflow.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
-from . import activity
+from .activity import Activity
 from . import canvas
 from . import task
 
@@ -17,12 +17,12 @@ class Workflow(object):
     def __init__(self, executor):
         self._executor = executor
 
-    def submit(self, func, *args, **kwargs):
+    def submit(self, activity, *args, **kwargs):
         """
         Submit a function for asynchronous execution.
 
-        :param func: callable registered as an task.
-        :type  func: activity.Activity | task.ActivityTask | canvas.Group | canvas.Chain | workflow.Workflow
+        :param activity: callable registered as an task.
+        :type  activity: activity.Activity | task.ActivityTask | canvas.Group | canvas.Chain | workflow.Workflow
         :param *args: arguments passed to the task.
         :type  *args: Sequence.
         :param **kwargs: keyword-arguments passed to the task.
@@ -32,50 +32,50 @@ class Workflow(object):
             :rtype: Future.
 
         """
-        # If the func is a child workflow, call directly
+        # If the activity is a child workflow, call directly
         # the executor
-        if inspect.isclass(func) and issubclass(func, Workflow):
-            return self._executor.submit(func, *args, **kwargs)
-        elif isinstance(func, task.ActivityTask):
-            return self._executor.submit(func.activity, *func.args, **func.kwargs)
-        elif isinstance(func, activity.Activity):
-            return self._executor.submit(func, *args, **kwargs)
-        elif isinstance(func, (canvas.Group, canvas.Chain)):
-            return func.submit(self._executor)
+        if inspect.isclass(activity) and issubclass(activity, Workflow):
+            return self._executor.submit(activity, *args, **kwargs)
+        elif isinstance(activity, task.ActivityTask):
+            return self._executor.submit(activity.activity, *activity.args, **activity.kwargs)
+        elif isinstance(activity, Activity):
+            return self._executor.submit(activity, *args, **kwargs)
+        elif isinstance(activity, (canvas.Group, canvas.Chain)):
+            return activity.submit(self._executor)
         else:
-            raise TypeError('Bad type for {} func ({})'.format(
-                func,
-                type(func)
+            raise TypeError('Bad type for {} activity ({})'.format(
+                activity,
+                type(activity)
             ))
 
-    def map(self, func, iterable):
+    def map(self, activity, iterable):
         """
         Submit a function for asynchronous execution for each value of
         *iterable*.
 
-        :param func: callable registered as an task.
-        :type  func: task.ActivityTask | task.WorkflowTask.
+        :param activity: callable registered as an task.
+        :type  activity: task.ActivityTask | task.WorkflowTask.
         :param iterable: collections of arguments passed to the task.
         :type  iterable: Iterable.
 
         """
-        group = canvas.Group(*[task.ActivityTask(func, i) for i in iterable])
+        group = canvas.Group(*[task.ActivityTask(activity, i) for i in iterable])
         return self.submit(group).futures
 
-    def starmap(self, func, iterable):
+    def starmap(self, activity, iterable):
         """
         Submit a function for asynchronous execution for each value of
         *iterable*.
 
-        :param func: callable registered as an task.
-        :type  func: task.ActivityTask | task.WorkflowTask.
+        :param activity: callable registered as an task.
+        :type  activity: task.ActivityTask | task.WorkflowTask.
         :param iterable: collections of multiple-arguments passed to the task
                          as positional arguments. They are destructured using
                          the ``*`` operator.
         :type  iterable: Iterable.
 
         """
-        group = canvas.Group(*[task.ActivityTask(func, *i) for i in iterable])
+        group = canvas.Group(*[task.ActivityTask(activity, *i) for i in iterable])
         return self.submit(group).futures
 
     def fail(self, reason, details=None):
