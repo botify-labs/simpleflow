eb5f0e87bc4d0f2dbf1607ba08a4c20b7bc6ee4c
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2016-08-29 11:19:25 +0200

    Improve WorkflowExecutionQueryset default date values
    The previous value of "30" was totally arbitrary and provoked bugs in
    client code that are hard to debug (a feature that was working fine for
    30 days but failed on the 31th day of a workflow). Better have a more
    intuitive value that, by default, retrieves everything.

--- swf/constants.py
+++ swf/constants.py
@@ -12,3 +12,9 @@ MAX_REASON_LENGTH = 256
 MAX_DETAILS_LENGTH = 32768
 MAX_INPUT_LENGTH = 32000
 MAX_RESULT_LENGTH = 32000
+
+# A SWF workflow cannot last more than a year, and workflows informations are
+# accessible for maximum 90 days (retention set at domain creation).
+# Hence this value is an upper limit to retrieve *all* open/closed workflow
+# executions on a given region+domain.
+MAX_WORKFLOW_AGE = 366 + 90 + 1
