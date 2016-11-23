8400509f1cdfd8fad5dc3d16543b934c9e5751b6
Yves Bastide <yves@botify.com>
2016-11-23 14:44:24 +0100

    Logging: add exception information
    Issue #162
    
    Just cut and pasted from logging.py.
    
    Signed-off-by: Yves Bastide <yves@botify.com>

--- simpleflow/settings/logging_formatter.py
+++ simpleflow/settings/logging_formatter.py
@@ -60,4 +60,27 @@ class SimpleflowFormatter(logging.Formatter):
         record.isodate = date.isoformat()
         record.message = record.msg % record.args
         record.coloredlevel = colorize(record.levelname, record.levelname)
-        return "%(isodate)s %(coloredlevel)s [process=%(processName)s, pid=%(process)s]: %(message)s" % record.__dict__
+        s = "%(isodate)s %(coloredlevel)s [process=%(processName)s, pid=%(process)s]: %(message)s" % record.__dict__
+
+        # C&P from logging.Formatter#format
+        if record.exc_info:
+            # Cache the traceback text to avoid converting it multiple times
+            # (it's constant anyway)
+            if not record.exc_text:
+                record.exc_text = self.formatException(record.exc_info)
+        if record.exc_text:
+            if s[-1:] != "\n":
+                s = s + "\n"
+            try:
+                s = s + record.exc_text
+            except UnicodeError:
+                # Sometimes filenames have non-ASCII chars, which can lead
+                # to errors when s is Unicode and record.exc_text is str
+                # See issue 8924.
+                # We also use replace for when there are multiple
+                # encodings, e.g. UTF-8 for the filesystem and latin-1
+                # for a script. See issue 13232.
+                s = s + record.exc_text.decode(sys.getfilesystemencoding(),
+                                               'replace')
+
+        return s
