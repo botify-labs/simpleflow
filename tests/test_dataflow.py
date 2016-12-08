ea2c38a2a29548dc2bfc6738658128d747fa9669
Yves Bastide <yves@botify.com>
2016-12-08 16:54:07 +0100

    Fix tests after rebase
    Signed-off-by: Yves Bastide <yves@botify.com>

--- tests/test_dataflow.py
+++ tests/test_dataflow.py
@@ -837,7 +837,7 @@ def test_workflow_with_child_workflow_failed():
     executor = Executor(DOMAIN, workflow)
     history = builder.History(workflow, input={'args': (1,)})
 
-    decisions, _ = executor.replay(Response(history=history))
+    decisions, _ = executor.replay(Response(history=history, execution=None))
     # Let's add the child workflow to the history to simulate its completion.
     (history
         .add_child_workflow(
@@ -849,7 +849,7 @@ def test_workflow_with_child_workflow_failed():
     ))
     # The child workflow fails and the executor should fail the
     # main workflow.
-    decisions, _ = executor.replay(Response(history=history))
+    decisions, _ = executor.replay(Response(history=history, execution=None))
     fail_workflow = swf.models.decision.WorkflowExecutionDecision()
     fail_workflow.fail(reason='FAIL')
 
@@ -864,7 +864,7 @@ def test_workflow_with_child_workflow_timed_out():
     executor = Executor(DOMAIN, workflow)
     history = builder.History(workflow, input={'args': (1,)})
 
-    decisions, _ = executor.replay(Response(history=history))
+    decisions, _ = executor.replay(Response(history=history, execution=None))
     # Let's add the child workflow to the history to simulate its completion.
     (history
         .add_child_workflow(
@@ -876,7 +876,7 @@ def test_workflow_with_child_workflow_timed_out():
     ))
     # The child workflow fails and the executor should fail the
     # main workflow.
-    decisions, _ = executor.replay(Response(history=history))
+    decisions, _ = executor.replay(Response(history=history, execution=None))
     fail_workflow = swf.models.decision.WorkflowExecutionDecision()
     fail_workflow.fail(reason='timed out')
 
@@ -891,7 +891,7 @@ def test_workflow_with_child_workflow_canceled():
     executor = Executor(DOMAIN, workflow)
     history = builder.History(workflow, input={'args': (1,)})
 
-    decisions, _ = executor.replay(Response(history=history))
+    decisions, _ = executor.replay(Response(history=history, execution=None))
     # Let's add the child workflow to the history to simulate its completion.
     (history
         .add_child_workflow(
@@ -903,7 +903,7 @@ def test_workflow_with_child_workflow_canceled():
     ))
     # The child workflow fails and the executor should fail the
     # main workflow.
-    decisions, _ = executor.replay(Response(history=history))
+    decisions, _ = executor.replay(Response(history=history, execution=None))
     fail_workflow = swf.models.decision.WorkflowExecutionDecision()
     fail_workflow.cancel()
 
@@ -918,7 +918,7 @@ def test_workflow_with_child_workflow_terminated():
     executor = Executor(DOMAIN, workflow)
     history = builder.History(workflow, input={'args': (1,)})
 
-    decisions, _ = executor.replay(Response(history=history))
+    decisions, _ = executor.replay(Response(history=history, execution=None))
     # Let's add the child workflow to the history to simulate its completion.
     (history
         .add_child_workflow(
@@ -930,7 +930,7 @@ def test_workflow_with_child_workflow_terminated():
     ))
     # The child workflow fails and the executor should fail the
     # main workflow.
-    decisions, _ = executor.replay(Response(history=history))
+    decisions, _ = executor.replay(Response(history=history, execution=None))
     fail_workflow = swf.models.decision.WorkflowExecutionDecision()
     fail_workflow.terminate()
 
@@ -1580,7 +1580,7 @@ def test_workflow_task_naming():
     workflow = ATestDefinitionParentWorkflow
     executor = Executor(DOMAIN, workflow)
     history = builder.History(workflow, input={})
-    decisions, _ = executor.replay(Response(history=history))
+    decisions, _ = executor.replay(Response(history=history, execution=None))
     assert decisions == [
         {
             'decisionType': 'StartChildWorkflowExecution',
@@ -1627,7 +1627,7 @@ def test_workflow_idempotent_task_naming():
     workflow = ATestDefinitionIdempotentParentWorkflow
     executor = Executor(DOMAIN, workflow)
     history = builder.History(workflow, input={})
-    decisions, _ = executor.replay(Response(history=history))
+    decisions, _ = executor.replay(Response(history=history, execution=None))
     assert decisions == [
         {
             'decisionType': 'StartChildWorkflowExecution',
