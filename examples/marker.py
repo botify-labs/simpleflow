e868f87b460c83265f7500bc62184a772f7de98c
Yves Bastide <yves@botify.com>
2017-02-23 16:51:28 +0100

    Implement markers for local.executor
    Also add an example.
    
    Signed-off-by: Yves Bastide <yves@botify.com>

---
+++ examples/marker.py
@@ -0,0 +1,14 @@
+from simpleflow import Workflow, futures
+
+
+class MarkerWorkflow(Workflow):
+    name = 'example'
+    version = 'example'
+    task_list = 'example'
+
+    def run(self):
+        m = self.submit(self.record_marker('marker 1'))
+        m = self.submit(self.record_marker('marker 1', 'some details'))
+        self.submit(self.record_marker('marker 2', "2nd marker's details"))
+        futures.wait(m)
+        print('Markers: {}'.format(self.list_markers()))
