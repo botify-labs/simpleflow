fbb2f90c497210b0d07d9b3135a494e22dea2133
ampelmann <thomas@botify.com>
2017-03-13 09:25:12 +0000

    use callable getter

--- simpleflow/swf/executor.py
+++ simpleflow/swf/executor.py
@@ -203,8 +203,8 @@ class Executor(executor.Executor):
         :rtype: str
 
         """
-        if isinstance(a_task, ActivityTask) and hasattr(a_task.activity._callable, 'get_task_id'):
-            suffix = a_task.activity._callable.get_task_id(self.workflow, *args, **kwargs)
+        if isinstance(a_task, ActivityTask) and hasattr(a_task.activity.callable, 'get_task_id'):
+            suffix = a_task.activity.callable.get_task_id(self.workflow, *args, **kwargs)
         elif not a_task.idempotent:
             # If idempotency is False or unknown, let's generate a task id by
             # incrementing an id after the a_task name.
