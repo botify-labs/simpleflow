8e0c9e4c4f51ab03f3d9027459bc9bab685b732a
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2017-05-22 07:03:28 +0200

    Add comment in swf.constants about probable wrong max field size

--- swf/constants.py
+++ swf/constants.py
@@ -13,6 +13,9 @@ MAX_DETAILS_LENGTH = 32768
 MAX_EXECUTION_CONTEXT_LENGTH = 32768
 MAX_HEARTBEAT_DETAILS_LENGTH = 2048
 MAX_IDENTITY_LENGTH = 256
+# TODO: experiment with the 2 following limits:
+# - the general limits doc states they're 32000: http://docs.aws.amazon.com/amazonswf/latest/developerguide/swf-dg-limits.html
+# - the API docs state they're 32768: http://docs.aws.amazon.com/amazonswf/latest/apireference/API_StartWorkflowExecution.html etc.
 MAX_INPUT_LENGTH = 32000
 MAX_RESULT_LENGTH = 32000
 
