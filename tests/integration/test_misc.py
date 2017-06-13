d09b5699f33f88c460857dd6615a154a86e62f01
Yves Bastide <yves@botify.com>
2017-06-13 18:40:53 +0200

    Add tests
    Signed-off-by: Yves Bastide <yves@botify.com>

---
+++ tests/integration/test_misc.py
@@ -0,0 +1,21 @@
+import json
+
+from sure import expect
+
+from tests.integration import VCRIntegrationTest, vcr
+
+
+class TestMisc(VCRIntegrationTest):
+    @vcr.use_cassette
+    def test_cancel_requested(self):
+        events = self.run_standalone('tests.integration.workflow.WorkflowToCancel', True)
+        cancel_requested = [e for e in events if e['eventType'] == 'WorkflowExecutionCancelRequested']
+        expect(len(cancel_requested)).to.equal(1)
+        expect(events[-1]['eventType']).to.equal('WorkflowExecutionCanceled')
+
+    @vcr.use_cassette
+    def test_cancel_requested_refused(self):
+        events = self.run_standalone('tests.integration.workflow.WorkflowToCancel', False)
+        cancel_requested = [e for e in events if e['eventType'] == 'WorkflowExecutionCancelRequested']
+        expect(len(cancel_requested)).to.equal(1)
+        expect(events[-1]['eventType']).to.equal('WorkflowExecutionCompleted')
