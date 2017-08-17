6393b20147def473f5fa1cacfdfa79ffb32fa514
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2017-08-17 14:14:30 +0200

    Bump version to 0.18.4

--- simpleflow/__init__.py
+++ simpleflow/__init__.py
@@ -1,17 +1,33 @@
 # -*- coding: utf-8 -*-
 
+
+
 import logging.config
 
+
+
 from .activity import Activity  # NOQA
+
 from .workflow import Workflow  # NOQA
+
 from .signal import WaitForSignal  # NOQA
+
+
 
 from . import settings
 
 
-__version__ = '0.18.3'
+
+
+
+__version__ = '0.18.4'
 __author__ = 'Greg Leclercq'
+
 __license__ = "MIT"
 
+
+
 logging.config.dictConfig(settings.base.load()['LOGGING'])
+
 logger = logging.getLogger(__name__)
+
