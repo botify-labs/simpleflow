54569a61c83e6c75f5d0db1279d816694af424c2
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2017-12-12 15:05:48 +0100

    Manage lazy download of binaries around activity tasks

---
+++ simpleflow/download.py
@@ -0,0 +1,82 @@
+import errno
+import functools
+import hashlib
+import logging
+import os
+
+from simpleflow.storage import pull
+
+
+LOCAL_BIN_DIR = "/tmp/simpleflow-binaries"
+
+
+logger = logging.getLogger(__name__)
+
+
+class RemoteBinary(object):
+    """
+    This class identifies if binary is available in $PATH so it can be used.
+    If the binary is not available, simpleflow will try to download it and
+    put it in a dedicated folder so it can be used. It will also prepend this
+    folder to $PATH before forking to the real activity worker.
+    """
+    def __init__(self, name, remote_location):
+        """
+        :param name: name of the binary to be downloaded
+        :type  name: str
+        :param remote_location: remote location where to download the binary from (only S3 for now)
+        :type  remote_location: str
+        """
+        self.name = name
+
+        # limit ourselves to S3 for now
+        assert remote_location.startswith("s3://")
+
+        self.remote_location = remote_location
+        self.local_directory = self._compute_local_directory()
+        self.local_location = self._compute_local_location()
+
+    def download(self):
+        self._mkdir_p(self.local_directory)
+        if not self._check_binary_present():
+            self._download_binary()
+
+    def _mkdir_p(self, path):
+        try:
+            os.makedirs(path)
+        except OSError as exc:
+            if exc.errno == errno.EEXIST and os.path.isdir(path):
+                pass
+            else:
+                raise
+
+    def _compute_local_directory(self):
+        suffix = hashlib.md5(self.remote_location.encode("utf-8")).hexdigest()
+        return os.path.join(LOCAL_BIN_DIR, "{}-{}".format(self.name, suffix))
+
+    def _compute_local_location(self):
+        return os.path.join(self.local_directory, self.name)
+
+    def _check_binary_present(self):
+        return os.access(self.local_location, os.X_OK)
+
+    def _download_binary(self):
+        logger.info("Downloading binary: {} -> {}".format(self.remote_location, self.local_location))
+        bucket, path = self.remote_location.replace("s3://", "", 1).split("/", 1)
+        # with FileLock(dest):
+        pull(bucket, path, self.local_location)
+        os.chmod(self.local_location, 0o755)
+
+
+# convenience helper
+def with_binaries(binaries_map):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            for binary, remote_location in binaries_map.items():
+                binary = RemoteBinary(binary, remote_location)
+                binary.download()
+                os.environ["PATH"] = binary.local_directory + ":" + os.environ["PATH"]
+            return func(*args, **kwargs)
+        return wrapped
+    return decorator
---
+++ tests/test_simpleflow/test_download.py
@@ -0,0 +1,66 @@
+import os
+import unittest
+from mock import patch
+import shutil
+from sure import expect
+
+from simpleflow.download import RemoteBinary, with_binaries
+
+
+# example binary remote/local location
+remote_location = "s3://a.bucket/v1.2.3/custom-bin"
+local_directory = "/tmp/simpleflow-binaries/custom-bin-585b3e5c252d6ec7aff52c24b149d719"
+local_location = "/tmp/simpleflow-binaries/custom-bin-585b3e5c252d6ec7aff52c24b149d719/custom-bin"
+
+
+def fake_download_binary(binary):
+    os.makedirs(binary.local_directory)
+    with open(binary.local_location, "a"):
+        os.utime(binary.local_location, None)  # aka "touch"
+        os.chmod(binary.local_location, 0o755)
+
+
+class TestRemoteBinary(unittest.TestCase):
+    def setUp(self):
+        self._cleanup()
+
+    def tearDown(self):
+        self._cleanup()
+
+    def _cleanup(self):
+        shutil.rmtree(local_directory, ignore_errors=True)
+
+    def test_locations_computing(self):
+        binary = RemoteBinary("custom-bin", remote_location)
+        expect(binary.local_directory).to.equal(local_directory)
+        expect(binary.local_location).to.equal(local_location)
+
+    @patch("simpleflow.download.RemoteBinary._download_binary")
+    def test_should_do_nothing_if_binary_present(self, method_mock):
+        binary = RemoteBinary("custom-bin", remote_location)
+        fake_download_binary(binary)
+
+        # binary already here and executable, we should *not* call _download_binary
+        binary.download()
+        expect(method_mock.call_count).to.equal(0)
+
+    @patch("simpleflow.download.RemoteBinary._download_binary", return_value=None)
+    def test_should_download_the_binary_if_needed(self, method_mock):
+        binary = RemoteBinary("custom-bin", remote_location)
+
+        # binary is not here, we should call _download_binary
+        binary.download()
+        method_mock.assert_called_once_with()
+
+
+class TestWithBinariesDecorator(unittest.TestCase):
+    @with_binaries({ "custom-bin": remote_location })
+    def method_needing_custom_binary(self):
+        return "foo!"
+
+    @patch("simpleflow.download.RemoteBinary._download_binary", return_value=None)
+    def test_with_binaries_decorator(self, method_mock):
+        res = self.method_needing_custom_binary()
+        expect(res).to.equal("foo!")
+
+        method_mock.assert_called_once_with()
