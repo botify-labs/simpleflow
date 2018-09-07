cb0733ce7fc20ca2ac8b71056a00dc06a57eb8d5
Bhaskar Mookerji <mookerji@secondmeasure.com>
2018-09-06 22:34:19 -0700

    Remove explicit handling of AWS credentials from env vars.
    Removes explicit handling of AWS keys from the environment, which is
    handled by boto2's credentials provider by default. This will also
    allow AWS_SECURITY_TOKEN to be handled correctly, which is broken in
    the boto2 v2.49 SWF Layer1 client.
    
    Closes https://github.com/botify-labs/simpleflow/issues/346.

--- tests/test_swf/test_settings.py
+++ tests/test_swf/test_settings.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 
+from swf.core import ConnectedSWFObject
 from swf.settings import from_env
 
 AWS_ENV_KEYS = (
@@ -24,18 +25,26 @@ class TestSettings(unittest.TestCase):
             else:
                 os.environ.pop(key, None)
 
+    def test_get_aws_settings_with_region(self):
+        """
+        AWS_DEFAULT_REGION is parsed correctly for settings.
+        """
+        os.environ["AWS_DEFAULT_REGION"] = "eu-west-1"
+        _settings = from_env()
+        self.assertEqual(_settings, {
+            "region": "eu-west-1"
+        })
+
     def test_get_aws_settings_with_access_key_id(self):
         """
-        If AWS_ACCESS_KEY_ID is set, get all 3 params from env.
+        Even if AWS_ACCESS_KEY_ID is set in env, don't pass it to settings.
         """
         os.environ["AWS_ACCESS_KEY_ID"] = "foo"
         os.environ["AWS_SECRET_ACCESS_KEY"] = "bar"
         os.environ["AWS_DEFAULT_REGION"] = "eu-west-1"
         _settings = from_env()
         self.assertEqual(_settings, {
-            "aws_access_key_id": "foo",
-            "aws_secret_access_key": "bar",
-            "region": "eu-west-1",
+            "region": "eu-west-1"
         })
 
     def test_get_aws_settings_without_access_key_id(self):
@@ -48,3 +57,17 @@ class TestSettings(unittest.TestCase):
         self.assertEqual(from_env(), {
             "region": "eu-west-1",
         })
+
+    def test_get_aws_connection_with_key(self):
+        """
+        If AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, and/or AWS_SECURITY_TOKEN
+        are set in environment, they are present in the boto connection.
+
+        """
+        os.environ["AWS_ACCESS_KEY_ID"] = "foo"
+        os.environ["AWS_SECRET_ACCESS_KEY"] = "bar"
+        os.environ["AWS_SECURITY_TOKEN"] = "baz"
+        obj = ConnectedSWFObject()
+        self.assertEqual(obj.connection.aws_access_key_id, "foo")
+        self.assertEqual(obj.connection.aws_secret_access_key, "bar")
+        self.assertEqual(obj.connection.provider.security_token, "baz")
