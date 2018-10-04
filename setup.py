2ccc98171b18f8460ea2fcee8d15f62a0bc5acd5
Yves Bastide <yves@botify.com>
2018-10-04 11:49:26 +0200

    Update Programming Language
    Signed-off-by: Yves Bastide <yves@botify.com>

--- setup.py
+++ setup.py
@@ -151,6 +151,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     test_suite='tests',
     tests_require=tests_require,
