383eace7d69d8ee9b2f01ebcf07b8e2f4f362979
Yves Bastide <yves@botify.com>
2020-09-16 12:47:06 +0200

    Bump version to 0.21.14a3

--- simpleflow/__init__.py
+++ simpleflow/__init__.py
@@ -4,6 +4,6 @@ from .runtime import logger  # NOQA
 from .signal import WaitForSignal  # NOQA
 from .workflow import Workflow  # NOQA
 
-__version__ = "0.21.14a2"
+__version__ = "0.21.14a3"
 __author__ = 'Greg Leclercq'
 __license__ = "MIT"
