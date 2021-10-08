7df35f86106833ca9f28de545a95892241c23f3a
Thomas Grange <thomas@botify.com>
2021-10-08 16:52:00 +0200

    fix

--- simpleflow/canvas.py
+++ simpleflow/canvas.py
@@ -238,7 +238,10 @@ class DynamicActivitiesBuilder(SubmittableContainer):
             {"name": activity.name, "args": activity.args, "kwargs": activity.kwargs}
             for activity in activity_tasks
         ]
-        fn = with_attributes(task_list=self.task_list, schedule_to_close_timeout=6 * HOUR)(execute_multiple_activities)
+        fn = with_attributes(
+            task_list=self.task_list,
+            schedule_to_start_timeout=6 * HOUR,
+            schedule_to_close_timeout=6 * HOUR)(execute_multiple_activities)
         return ActivityTask(fn, args)
 
     def submit(self, executor):
