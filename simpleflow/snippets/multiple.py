0e4f1adec1f74d98a75f5a1a6fc5c08aaa4fe80d
Thomas Grange <thomas@botify.com>
2021-10-08 15:32:59 +0200

    WIP

--- simpleflow/snippets/multiple.py
+++ simpleflow/snippets/multiple.py
@@ -1,22 +1,23 @@
-import json
-
-from simpleflow import logger
+from simpleflow import logger, Activity
+from simpleflow.task import ActivityTask
 from simpleflow.utils import import_from_module
 
 
 def execute_multiple_activities(*args, **kwargs):
     print("execute multiple activities", args, kwargs)
     results = []
-    for activity in args:
+    for activity in args[0]:
         funcname = activity["name"].replace("activity-", "")
-        logger.info("func is", funcname)
-        func = import_from_module()
-        logger.info("func is", func)
+        logger.info("func is" + funcname)
+        func = import_from_module(funcname)
+        #logger.info("func is", func)
 
-        if isinstance(func, object):
-            result = func(activity["args"], activity["kwargs"]).run()
+        if isinstance(func, Activity):
+            result = func.callable(*activity["args"], **activity["kwargs"])
+        elif isinstance(func, ActivityTask):
+            result = func(*activity["args"], **activity["kwargs"]).execute()
         else:
-            result = func(activity["args"], activity["kwargs"])
+            raise ValueError
         results.append(result)
 
     return results
