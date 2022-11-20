c26f1534d5f0079ed3ddf4322aea5b49cd164bc6
Yves Bastide <yves@botify.com>
2022-11-20 12:42:22 +0100

    Update version to 0.30.0dev
    Signed-off-by: Yves Bastide <yves@botify.com>

--- simpleflow/__init__.py
+++ simpleflow/__init__.py
@@ -5,6 +5,6 @@ from .runtime import logger  # NOQA
 from .signal import WaitForSignal  # NOQA
 from .workflow import Workflow  # NOQA
 
-__version__ = "0.21.18"
+__version__ = "0.30.0.dev2"
 __author__ = "Greg Leclercq"
 __license__ = "MIT"
