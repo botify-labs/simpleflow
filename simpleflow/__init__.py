bd61116b8345a609301ac846a5d421aaa68ec0e9
Yves Bastide <yves@botify.com>
2023-07-25 09:49:31 +0200

    Revert "chore: increase __version__"
    This reverts commit dc9d047c4afb8a49bb18b702526fed1fbb9e14e8.

--- simpleflow/__init__.py
+++ simpleflow/__init__.py
@@ -5,6 +5,6 @@ from .runtime import logger  # NOQA
 from .signal import WaitForSignal  # NOQA
 from .workflow import Workflow  # NOQA
 
-__version__ = "0.32.1dev1"
+__version__ = "0.32.0"
 __author__ = "Greg Leclercq"
 __license__ = "MIT"
